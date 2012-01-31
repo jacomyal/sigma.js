9dd80d5381636c585d48f7bef207b0f3dfdb6f24
Alexis Jacomy <alexis.jacomy@gmail.com>
2012-02-01 00:07:19 +0100

    mousecaptor update: drag'n'drop available, zooming WIP (not working yet, falling asleep, will do it lateeeer...)

--- src/core/mousecaptor.js
+++ src/core/mousecaptor.js
@@ -1,9 +1,10 @@
-function MouseCaptor(canvas, graph) {
+function MouseCaptor(canvas, graph, id) {
   var self = this;
   sigma.classes.EventDispatcher.call(this);
 
   this.canvas = canvas;
   this.graph = graph;
+  this.id = id;
 
   // MOUSE
   this.mouseX = 0;
@@ -17,9 +18,18 @@ function MouseCaptor(canvas, graph) {
   this.triggerDrag = false;
 
   // SCENE
+  this.oldStageX = 0;
+  this.oldStageY = 0;
+  this.oldRatio = 1;
   this.stageX = 0;
   this.stageY = 0;
   this.ratio = 1;
+
+  this.targetStageX = 0;
+  this.targetStageY = 0;
+  this.targetRatio = 1;
+
+  this.progress = 0;
 
   // CALLBACKS
   function moveHandler(event) {
@@ -49,30 +59,88 @@ function MouseCaptor(canvas, graph) {
   };
 
   function wheelHandler(event) {
-    self.ratio += event.wheelDelta / 10;
+    startZooming(event.wheelDelta);
   };
 
   // CUSTOM ACTIONS
-  function startDrag(event) {
+  function startDrag() {
     self.startX = self.mouseX;
     self.startY = self.mouseY;
 
-    self.dispatch('startdrag', event);
+    self.dispatch('startdrag');
   };
 
-  function stopDrag(event) {
-    // TODO
-    self.dispatch('stopdrag', event);
+  function stopDrag() {
+    self.oldStageX = self.stageX;
+    self.oldStageY = self.stageY;
+    self.dispatch('stopdrag');
   };
 
-  function drag(event) {
-    self.dispatch('drag', {
-      dx: self.mouseX - self.startX,
-      dy: self.mouseY - self.startY,
-      x: self.mouseX,
-      y: self.mouseY,
-      ratio: self.ratio
-    });
+  function drag() {
+    self.stageX = self.mouseX - self.startX + self.oldStageX;
+    self.stageY = self.mouseY - self.startY + self.oldStageY;
+
+    self.dispatch('drag');
+  };
+
+  function startZooming(delta) {
+    self.oldRatio = self.ratio;
+    self.targetRatio = self.ratio * (delta > 0 ? 2 : 1 / 2);
+    self.progress = 0;
+
+    /*sigma.scheduler.addWorker(
+      zooming,
+      'zooming_'+self.id,
+      false
+    ).queueWorker(
+      stopZooming,
+      'stopzooming_'+self.id,
+      'zooming_'+self.id
+    ).start();*/
+
+    self.zoomID = window.setInterval(zooming, 50);
+  };
+
+  function stopZooming() {
+    var oldRatio = self.ratio;
+
+    self.ratio = self.targetRatio;
+    self.stageX = self.mouseX +
+                  (self.stageX - self.mouseX) *
+                  self.ratio /
+                  self.ratio;
+    self.stageY = self.mouseY +
+                  (self.stageY - self.mouseY) *
+                  self.ratio /
+                  self.ratio;
+
+    self.dispatch('stopzooming');
+    return false;
+  };
+
+  function zooming() {
+    self.progress += 0.05;
+    var k = sigma.easing.quadratic.easeinout(self.progress);
+    var oldRatio = self.ratio;
+
+    self.ratio = self.oldRatio * (1 - k) + self.targetRatio * k;
+    self.stageX = self.mouseX +
+                  (self.stageX - self.mouseX) *
+                  self.ratio /
+                  self.ratio;
+    self.stageY = self.mouseY +
+                  (self.stageY - self.mouseY) *
+                  self.ratio /
+                  self.ratio;
+
+    dispatch('zooming');
+
+    //return self.progress<1;
+
+    if (self.progress > 1) {
+      window.clearInterval(self.zoomID);
+      window.setTimeout(stopZooming, 50);
+    }
   };
 
   // ADD CALLBACKS
