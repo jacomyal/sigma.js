13069b34e95666831bd6a61f4e11ee0605fc84c3
ronp@winter <ron.peleg@trusteer.com>
2013-06-03 17:09:14 +0300

    added pluggable shape drawing to the nodeShape plug-in. currently the circle drawing from the original Plotter class are plugged as shape 'circle'

--- plugins/sigma.nodeShapes.js
+++ plugins/sigma.nodeShapes.js
@@ -11,32 +11,58 @@ sigma.nodeShapes.NodeShapes = function(graph,plotter) {
     /* TODO add configuration here */
   };
 
-  this.originalFunctions = {};
+  var shapeFunctions = {};
 
   this.init = function() {
-    // mainain original node-drawing functions
-    self.originalFunctions['drawNodeShape']   = plotter.drawNodeShape;
-    self.originalFunctions['drawHoverNodeBorder']  = plotter.drawHoverNodeBorder;
-    self.originalFunctions['drawHoverNodeShape']  = plotter.drawHoverNodeShape;
-    self.originalFunctions['drawActiveNodeBorder']  = plotter.drawActiveNodeBorder;
-    self.originalFunctions['drawActiveNodeShape']  = plotter.drawActiveNodeShape;
+    // mainain original node-drawing functions, as shape drawing for 'circle'
+    self.addShapeFunctions('circle', {
+      'drawNodeShape': plotter.drawNodeShape,
+      'drawHoverNodeBorder': plotter.drawHoverNodeBorder,
+      'drawHoverNodeShape': plotter.drawHoverNodeShape,
+      'drawActiveNodeBorder': plotter.drawActiveNodeBorder,
+      'drawActiveNodeShape': plotter.drawActiveNodeShape
+    });
 
-    // override node-drawing
-    plotter.drawNodeShape   = drawShapedNode;
-    // plotter.drawHoverNodeBorder  = drawShapedNodeBorder;
-    plotter.drawHoverNodeShape  = drawShapedNode;
-    // plotter.drawActiveNodeBorder  = drawShapedNodeBorder;
-    plotter.drawActiveNodeShape  = drawShapedNode;
+    // override node-drawing, with wrappers that switch on node.attr.shape,
+    // and add images on top of the non-border drawing functions
+    plotter.drawNodeShape   = drawNodeShapeWImage;
+    plotter.drawHoverNodeBorder  = drawHoverNodeShapeBorder;
+    plotter.drawHoverNodeShape  = drawHoverNodeShapeWImage;
+    plotter.drawActiveNodeBorder  = drawActiveNodeShapeBorder;
+    plotter.drawActiveNodeShape  = drawActiveNodeShapeWImage;
   };
 
   this.finalize = function() {
     // restore original node-drawing functions
-    plotter.drawNodeShape   = self.originalFunctions['drawNodeShape'];
-    plotter.drawHoverNodeBorder  = self.originalFunctions['drawHoverNodeBorder'];
-    plotter.drawHoverNodeShape  = self.originalFunctions['drawHoverNodeShape'];
-    plotter.drawActiveNodeBorder  = self.originalFunctions['drawActiveNodeBorder'];
-    plotter.drawActiveNodeShape  = self.originalFunctions['drawActiveNodeShape'];
+    plotter.drawNodeShape   = shapeFunctions['circle']['drawNodeShape'];
+    plotter.drawHoverNodeBorder  = shapeFunctions['circle']['drawHoverNodeBorder'];
+    plotter.drawHoverNodeShape  = shapeFunctions['circle']['drawHoverNodeShape'];
+    plotter.drawActiveNodeBorder  = shapeFunctions['circle']['drawActiveNodeBorder'];
+    plotter.drawActiveNodeShape  = shapeFunctions['circle']['drawActiveNodeShape'];
   };
+
+  /**
+   * Sets the drawing functions for a specific shape
+   * @param {string} name of shape, to be used on node.attr.shape
+   * @param {object} hash of functions. the following keys are expected:
+   *                 - drawNodeShape
+   *                 - drawHoverNodeBorder
+   *                 - drawHoverNodeShape
+   *                 - drawActiveNodeBorder
+   *                 - drawActiveNodeShape
+   *    each functions draws the expected node shape to the corresponding canvas,
+   *    following the below prototype:
+   *      @param  {Object} canvas context to draw on
+   *      @param  {string} designated color
+   *      @param  {float}  display X for center
+   *      @param  {float}  display Y for center
+   *      @param  {float}  display size - all objects pixels must be inside
+   *      @param  {Object} node The node to draw - node date is used to determine shape
+   *              and image to draw
+   */
+  this.addShapeFunctions = function(name,funcs) {
+    shapeFunctions[name]=funcs;
+  }
 
   /**
    * Cache for IMG objects
@@ -44,32 +70,11 @@ sigma.nodeShapes.NodeShapes = function(graph,plotter) {
    */
   var imgCache = {};
 
-
   /**
-   * Draws one filled node shape to the corresponding canvas.
-   * @param  {Object} canvas context to draw on
-   * @param  {string} designated color
-   * @param  {float}  display X for center
-   * @param  {float}  display Y for center
-   * @param  {float}  display size - all objects pixels must be inside
-   * @param  {Object} node The node to draw - node date is used to determine shape
-   *        and image to draw
+   * draws image on node
+   * @see this.addShapeFunctions for explanation on function prototype
    */
-  function drawShapedNode(ctx, color, x, y, size, node) {
-    // At this point only draws filled circle, will add more shapes
-    ctx.fillStyle = color;
-    ctx.beginPath();
-    ctx.arc(x,
-        y,
-        size,
-        0,
-        Math.PI * 2,
-        true);
-
-    ctx.closePath();
-    ctx.fill();
-
-    // Use node data to draw image inside shape
+  function drawImageOnNode(ctx, color, x, y, size, node) {
     if(node.attr.image) {
       var url = node.attr.image.url;
       var ih = node.attr.image.h;
@@ -95,6 +100,53 @@ sigma.nodeShapes.NodeShapes = function(graph,plotter) {
     }
   }
 
+  /**
+   * wrapper for drawNodeShape, adds image on top
+   * @see this.addShapeFunctions for explanation on function prototype
+   */
+  function drawNodeShapeWImage(ctx, color, x, y, size, node) {
+    var functions = shapeFunctions[node.attr.shape] || shapeFunctions['circle'];
+    functions['drawNodeShape'](ctx, color, x, y, size, node);
+    drawImageOnNode(ctx, color, x, y, size, node);
+  }
+
+  /**
+   * wrapper for drawHoverNodeShape, adds image on top
+   * @see this.addShapeFunctions for explanation on function prototype
+   */
+  function drawHoverNodeShapeWImage(ctx, color, x, y, size, node) {
+    var functions = shapeFunctions[node.attr.shape] || shapeFunctions['circle'];
+    functions['drawHoverNodeShape'](ctx, color, x, y, size, node);
+    drawImageOnNode(ctx, color, x, y, size, node);
+  }
+
+  /**
+   * wrapper for drawHoverNodeBorder
+   * @see this.addShapeFunctions for explanation on function prototype
+   */
+  function drawHoverNodeShapeBorder(ctx, color, x, y, size, node) {
+    var functions = shapeFunctions[node.attr.shape] || shapeFunctions['circle'];
+    functions['drawHoverNodeBorder'](ctx, color, x, y, size, node);
+  }
+
+  /**
+   * wrapper for drawActiveNodeShape, adds image on top
+   * @see this.addShapeFunctions for explanation on function prototype
+   */
+  function drawActiveNodeShapeWImage(ctx, color, x, y, size, node) {
+    var functions = shapeFunctions[node.attr.shape] || shapeFunctions['circle'];
+    functions['drawActiveNodeShape'](ctx, color, x, y, size, node);
+    drawImageOnNode(ctx, color, x, y, size, node);
+  }
+
+  /**
+   * wrapper for drawActiveNodeBorder
+   * @see this.addShapeFunctions for explanation on function prototype
+   */
+  function drawActiveNodeShapeBorder(ctx, color, x, y, size, node) {
+    var functions = shapeFunctions[node.attr.shape] || shapeFunctions['circle'];
+    functions['drawActiveNodeBorder'](ctx, color, x, y, size, node);
+  }
 };
 
 sigma.publicPrototype.startNodeShapes = function() {
