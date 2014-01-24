25b3b8c950504fdd3c6ae63e87693ae6870335af
ronp@vagrant <ron@il.ibm.com>
2014-01-24 17:35:05 +0100

    first draft of plugin files and sample. See README for work to complete this

---
+++ plugins/sigma.renderers.customShapes/README.md
@@ -0,0 +1,13 @@
+sigma.renderers.customShapes
+==================
+
+Plugin developed by [Ron Peleg](https://github.com/rpeleg1970).
+
+---
+This plugin registers custom node shape renderers, work in progress
+
+TODOS:
+* Add images
+* Clean up the example page
+* Proper README
+* break out shapes to a separate shape-library file
---
+++ plugins/sigma.renderers.customShapes/sigma.renderers.customShapes.js
@@ -0,0 +1,120 @@
+;(function(undefined) {
+  'use strict';
+
+  if (typeof sigma === 'undefined')
+    throw 'sigma is not declared';
+
+  // Initialize package:
+  sigma.utils.pkg('sigma.canvas.nodes');
+
+  var register = function(name,drawShape,drawBorder) {
+    sigma.canvas.nodes[name] = function(node, context, settings) {
+      var args = arguments,
+          prefix = settings('prefix') || '',
+          size = node[prefix + 'size'],
+          color = node.color || settings('defaultNodeColor'),
+          borderColor = node.borderColor || color,
+          x = node[prefix + 'x'],
+          y = node[prefix + 'y'];
+
+      context.save();
+
+      // Draw shape
+      if(drawShape) {
+        context.beginPath();
+        context.fillStyle = color;
+        drawShape(node,x,y,size,context);
+        context.closePath();
+        context.fill();
+      }
+
+      // Draw the border:
+      if(drawBorder) {
+        context.beginPath();
+        context.lineWidth = size / 5;
+        context.strokeStyle = borderColor;
+        drawBorder(node,x,y,size,context);
+        context.closePath();
+        context.stroke();
+      }
+      
+      // TODO add image
+
+      context.restore();
+    };
+  }
+
+  var drawSquare = function(node,x,y,size,context) {
+    context.fillRect(
+        x+Math.sin(-3.142/4)*size,
+        y-Math.cos(-3.142/4)*size,
+        size*2*Math.sin(-3.142/4)*(-1),
+        size*2*Math.cos(-3.142/4));
+  }
+  register("square",drawSquare,drawSquare);
+
+  var drawDiamond = function(node,x,y,size,context) {
+    context.moveTo(x-size, y);
+    context.lineTo(x, y-size);
+    context.lineTo(x+size, y);
+    context.lineTo(x, y+size);
+    context.moveTo(x-size, y);
+  }
+  register("diamond",drawDiamond,drawDiamond);
+
+  var drawEquilateral = function(node,x,y,size,context) {
+    var pcount = (node.equilateral && node.equilateral.numPoints) || 5;
+    var rotate = ((node.equilateral && node.equilateral.rotate) || 0)*Math.PI/180;
+    var radius = size;
+    context.moveTo(x+radius*Math.sin(rotate), y-radius*Math.cos(rotate)); // first point on outer radius, angle 'rotate'
+    for(var i=1; i<pcount; i++) {
+      context.lineTo(x+Math.sin(rotate+2*Math.PI*i/pcount)*radius, y-Math.cos(rotate+2*Math.PI*i/pcount)*radius);
+    }
+  }
+  register("equilateral",drawEquilateral,drawEquilateral);
+
+
+  var starShape = function(node,x,y,size,context) {
+    var pcount = (node.star && node.star.numPoints) || 5,
+        inRatio = (node.star && node.star.innerRatio) || 0.5,
+        outR = size,
+        inR = size*inRatio,
+        angleOffset = Math.PI/pcount;
+    context.moveTo(x, y-size); // first point on outer radius, top
+    for(var i=0; i<pcount; i++) {
+      context.lineTo(x+Math.sin(angleOffset+2*Math.PI*i/pcount)*inR,
+          y-Math.cos(angleOffset+2*Math.PI*i/pcount)*inR);
+      context.lineTo(x+Math.sin(2*Math.PI*(i+1)/pcount)*outR,
+          y-Math.cos(2*Math.PI*(i+1)/pcount)*outR);
+    }
+  }
+  register("star",starShape,starShape);
+
+  var drawPacman = function(node,x,y,size,context) {
+    context.save();
+    context.fillStyle = 'yellow';
+    context.beginPath();
+    context.arc(x,y,size,1.25*Math.PI,0,false);
+    context.arc(x,y,size,0,0.75*Math.PI,false);
+    context.lineTo(x,y);
+    context.closePath();
+    context.fill();
+
+    context.fillStyle = 'white';
+    context.strokeStyle = 'black';
+    context.beginPath();
+    context.arc(x+size/3,y-size/3,size/4,0,2*Math.PI,false);
+    context.closePath();
+    context.fill();
+    context.stroke();
+
+    context.fillStyle = 'black';
+    context.beginPath();
+    context.arc(x+4*size/9,y-size/3,size/8,0,2*Math.PI,false);
+    context.closePath();
+    context.fill();
+    context.restore();
+  }
+  register("pacman",drawPacman,null);
+
+}).call(this);
