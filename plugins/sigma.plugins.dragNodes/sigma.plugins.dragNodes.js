bc7e776111623e80c08e61f22bf6a2f8d3ef3d92
José M. Camacho <hello@josemazo.com>
2014-03-25 13:36:28 -0400

    Fixed the size check and the dragging with rotated graphs

--- plugins/sigma.plugins.dragNodes/sigma.plugins.dragNodes.js
+++ plugins/sigma.plugins.dragNodes/sigma.plugins.dragNodes.js
@@ -1,7 +1,7 @@
 /**
  * This plugin provides a method to drag & drop nodes. Check the
  * sigma.plugins.dragNodes function doc or the examples/basic.html &
- * examples/api-candy.html code sample to know more.
+ * examples/api-candy.html code samples to know more.
  */
 (function() {
   'use strict';
@@ -15,7 +15,8 @@
    * This function will add `mousedown`, `mouseup` & `mousemove` events to the
    * nodes in the `overNode`event to perform drag & drop operations. It uses
    * `linear interpolation` [http://en.wikipedia.org/wiki/Linear_interpolation]
-   * to calculate the X and Y coordinates from the `cam` or `renderer` node
+   * and `rotation matrix` [http://en.wikipedia.org/wiki/Rotation_matrix] to
+   * calculate the X and Y coordinates from the `cam` or `renderer` node
    * attributes. These attributes represent the coordinates of the nodes in
    * the real container, not in canvas.
    *
@@ -32,33 +33,33 @@
    */
   sigma.plugins.dragNodes = function(s, renderer, realCoors) {
 
-    var _size = s.graph.nodes().length;
-    if (_size > 1) {
-      var _container = renderer.container,
-          _mouse = _container.lastChild,
-          _body = document.body,
-          _node = null,
-          _isOverNode = false,
-          _isMouseOverCanvas = false,
-          _realCoors = realCoors;
+    var _container = renderer.container,
+        _mouse = _container.lastChild,
+        _body = document.body,
+        _camera = renderer.camera,
+        _node = null,
+        _isOverNode = false,
+        _isMouseOverCanvas = false,
+        _realCoors = realCoors;
 
-      var nodeMouseOver = function(event) {
-        if (!_isOverNode) {
-          _node = event.data.node;
-          _mouse.addEventListener('mousedown', nodeMouseDown);
+    var nodeMouseOver = function(event) {
+      if (!_isOverNode) {
+        _node = event.data.node;
+        _mouse.addEventListener('mousedown', nodeMouseDown);
+        _isOverNode = true;
+      }
+    };
 
-          _isOverNode = true;
-        }
-      };
+    var treatOutNode = function(event) {
+      if (_isOverNode) {
+        _mouse.removeEventListener('mousedown', nodeMouseDown);
+        _isOverNode = false;
+      }
+    };
 
-      var treatOutNode = function(event) {
-        if (_isOverNode) {
-          _mouse.removeEventListener('mousedown', nodeMouseDown);
-          _isOverNode = false;
-        }
-      };
-
-      var nodeMouseDown = function(event) {
+    var nodeMouseDown = function(event) {
+      var size = s.graph.nodes().length;
+      if (size > 1) {
         _mouse.removeEventListener('mousedown', nodeMouseDown);
         _body.addEventListener('mousemove', nodeMouseMove);
         _body.addEventListener('mouseup', nodeMouseUp);
@@ -68,48 +69,57 @@
         // Deactivate drag graph.
         renderer.settings({mouseEnabled: false, enableHovering: false});
         s.refresh();
-      };
+      }
+    };
 
-      var nodeMouseUp = function(event) {
-        _mouse.addEventListener('mousedown', nodeMouseDown);
-        _body.removeEventListener('mousemove', nodeMouseMove);
-        _body.removeEventListener('mouseup', nodeMouseUp);
+    var nodeMouseUp = function(event) {
+      _mouse.addEventListener('mousedown', nodeMouseDown);
+      _body.removeEventListener('mousemove', nodeMouseMove);
+      _body.removeEventListener('mouseup', nodeMouseUp);
 
-        treatOutNode();
-        renderer.bind('outNode', treatOutNode);
-
-        // Activate drag graph.
-        renderer.settings({mouseEnabled: true, enableHovering: true});
-        s.refresh();
-      };
-
-      var nodeMouseMove = function(event) {
-        var x = event.pageX - _container.offsetLeft;
-        var y = event.pageY - _container.offsetTop;
-
-        var nodes = s.graph.nodes();
-        var ref = [];
-        for (var i = 0; i < 2; i++) {
-          var n = nodes[i];
-          var aux = {};
-          aux.x = n.x;
-          aux.y = n.y;
-          aux.renX = n[_realCoors + ':x'];
-          aux.renY = n[_realCoors + ':y'];
-          ref.push(aux);
-        }
-
-        _node.x = ((x - ref[0].renX) / (ref[1].renX - ref[0].renX)) *
-          (ref[1].x - ref[0].x) + ref[0].x;
-        _node.y = ((y - ref[0].renY) / (ref[1].renY - ref[0].renY)) *
-          (ref[1].y - ref[0].y) + ref[0].y;
-
-        s.refresh();
-      };
-
-      renderer.bind('overNode', nodeMouseOver);
+      treatOutNode();
       renderer.bind('outNode', treatOutNode);
-    }
+
+      // Activate drag graph.
+      renderer.settings({mouseEnabled: true, enableHovering: true});
+      s.refresh();
+    };
+
+    var nodeMouseMove = function(event) {
+      var x = event.pageX - _container.offsetLeft,
+          y = event.pageY - _container.offsetTop,
+          cos = Math.cos(_camera.angle),
+          sin = Math.sin(_camera.angle),
+          nodes = s.graph.nodes(),
+          ref = [];
+
+      // Getting and derotating the reference coordinates.
+      for (var i = 0; i < 2; i++) {
+        var n = nodes[i];
+        var aux = {
+          x: n.x * cos + n.y * sin,
+          y: n.y * cos - n.x * sin,
+          renX: n[_realCoors + ':x'],
+          renY: n[_realCoors + ':y'],
+        };
+        ref.push(aux);
+      }
+
+      // Applying linear interpolation.
+      x = ((x - ref[0].renX) / (ref[1].renX - ref[0].renX)) *
+        (ref[1].x - ref[0].x) + ref[0].x;
+      y = ((y - ref[0].renY) / (ref[1].renY - ref[0].renY)) *
+        (ref[1].y - ref[0].y) + ref[0].y;
+
+      // Rotating the coordinates.
+      _node.x = x * cos - y * sin;;
+      _node.y = y * cos + x * sin;;
+
+      s.refresh();
+    };
+
+    renderer.bind('overNode', nodeMouseOver);
+    renderer.bind('outNode', treatOutNode);
   };
 
 }).call(window);
