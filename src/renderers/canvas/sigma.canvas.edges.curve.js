a0db578202b5a5b4d4cbd4080b476d5da08e07f7
Sergey Sidorchick <whitekrokodile@gmail.com>
2014-05-09 11:55:02 +0300

    Add self referenced nodes edge rendering.

--- src/renderers/canvas/sigma.canvas.edges.curve.js
+++ src/renderers/canvas/sigma.canvas.edges.curve.js
@@ -17,7 +17,26 @@
         prefix = settings('prefix') || '',
         edgeColor = settings('edgeColor'),
         defaultNodeColor = settings('defaultNodeColor'),
-        defaultEdgeColor = settings('defaultEdgeColor');
+        defaultEdgeColor = settings('defaultEdgeColor'),
+        sSize = source[prefix + 'size'],
+        sX = source[prefix + 'x'],
+        sY = source[prefix + 'y'],
+        tX = target[prefix + 'x'],
+        tY = target[prefix + 'y'],
+        controlX,
+        controlY,
+        controlX2,
+        controlY2;
+
+    if (source.id === target.id) {
+      controlX = sX - sSize * 7;
+      controlY = sY;
+      controlX2 = sX;
+      controlY2 = sY + sSize * 7;
+    } else {
+      controlX = (sX + tX) / 2 + (tY - sY) / 4;
+      controlY = (sY + tY) / 2 + (sX - tX) / 4;
+    }
 
     if (!color)
       switch (edgeColor) {
@@ -35,18 +54,12 @@
     context.strokeStyle = color;
     context.lineWidth = edge[prefix + 'size'] || 1;
     context.beginPath();
-    context.moveTo(
-      source[prefix + 'x'],
-      source[prefix + 'y']
-    );
-    context.quadraticCurveTo(
-      (source[prefix + 'x'] + target[prefix + 'x']) / 2 +
-        (target[prefix + 'y'] - source[prefix + 'y']) / 4,
-      (source[prefix + 'y'] + target[prefix + 'y']) / 2 +
-        (source[prefix + 'x'] - target[prefix + 'x']) / 4,
-      target[prefix + 'x'],
-      target[prefix + 'y']
-    );
+    context.moveTo(sX, sY);
+    if (source.id === target.id) {
+      context.bezierCurveTo(controlX2, controlY2, controlX, controlY, tX, tY);
+    } else {
+      context.quadraticCurveTo(controlX, controlY, tX, tY);
+    }
     context.stroke();
   };
 })();
