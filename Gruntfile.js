ded2b1c18e7fd7fa101df9ee6944151e2174cb41
Sébastien Heymann <seb@linkurio.us>
2014-08-18 11:17:45 +0200

    Update Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -55,6 +55,7 @@ module.exports = function(grunt) {
   npmJsFiles.splice(2, 0, 'src/sigma.export.js');
 
   var plugins = [
+    'plugins.edgeSiblings',
     'layout.forceAtlas2',
     'parsers.gexf',
     'parsers.json',
