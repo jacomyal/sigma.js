5d95ea2754f41ad528097305034b0abd8285399e
Sébastien Heymann <seb@linkurio.us>
2014-09-01 13:37:34 +0200

    Lint utils

--- src/utils/sigma.utils.js
+++ src/utils/sigma.utils.js
@@ -272,7 +272,7 @@
     dy = y1 - y0;
 
     // Determine the straight-line distance between the centers:
-    d = Math.sqrt((dy*dy) + (dx*dx));
+    d = Math.sqrt((dy * dy) + (dx * dx));
 
     // Check for solvability:
     if (d > (r0 + r1)) {
@@ -288,19 +288,19 @@
     // points crosses the line between the circle centers.
 
     // Determine the distance from point 0 to point 2:
-    a = ((r0*r0) - (r1*r1) + (d*d)) / (2.0 * d) ;
+    a = ((r0 * r0) - (r1 * r1) + (d * d)) / (2.0 * d);
 
     // Determine the coordinates of point 2:
-    x2 = x0 + (dx * a/d);
-    y2 = y0 + (dy * a/d);
+    x2 = x0 + (dx * a / d);
+    y2 = y0 + (dy * a / d);
 
     // Determine the distance from point 2 to either of the intersection
     // points:
-    h = Math.sqrt((r0*r0) - (a*a));
+    h = Math.sqrt((r0 * r0) - (a * a));
 
     // Determine the offsets of the intersection points from point 2:
-    rx = -dy * (h/d);
-    ry = dx * (h/d);
+    rx = -dy * (h / d);
+    ry = dx * (h / d);
 
     // Determine the absolute intersection points:
     var xi = x2 + rx;
