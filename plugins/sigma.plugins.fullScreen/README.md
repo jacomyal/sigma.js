2a4f19b23df8153ff819605523eec95e58122536
Sébastien Heymann <seb@linkurio.us>
2014-10-10 10:55:20 +0200

    Refactor plugins.fullscreen

--- plugins/sigma.plugins.fullScreen/README.md
+++ plugins/sigma.plugins.fullScreen/README.md
@@ -1,37 +1,30 @@
 sigma.plugins.fullScreen
 =====================
 
-Plugin developed by [Martin de la Taille](https://github.com/martindelataille) for [Linkurious](https://github.com/Linkurious) and published under the [MIT](LICENSE) license.
+Plugin developed by [Martin de la Taille](https://github.com/martindelataille) and [Sébastien Heymann](https://github.com/sheymann) for [Linkurious](https://github.com/Linkurious) and published under the [MIT](LICENSE) license.
 
 ---
 
-This plugin provides a method to use fullScreen mode with or without a button
+This plugin provides a method to activate the fullscreen mode with a button.
 
-## General
-This plugin provides a method to use fullScreen mode with or without a button.
+See the following [example code](../../examples/plugin-fullscreen.html) for full usage.
 
-See the following [example code](../../examples/fullscreen.html) for full usage.
-
-To use, include all .js files under this folder. Then initialize it as follows with a button:
+To use, include all .js files under this folder. Then initialize it as follows with the id of a button element:
 
 ````javascript
-sigma.plugins.fullScreen(s, {
+myRenderer.fullScreen({
   id : 'graph-btn'
 });
 ````
 
-or without a button :
+The user may leave the fullscreen mode manually. Calling the method with no argument will leave the fullscreen mode as well :
 
 ````javascript
-sigma.plugins.fullScreen(s);
+myRenderer.fullScreen();
 ````
 
 Kill the plugin instance as follows:
 
 ````javascript
-sigma.plugins.killFullscreen();
+myRenderer.killFullScreen();
 ````
-
-## Status
-
-Beta
--- plugins/sigma.plugins.fullScreen/sigma.plugins.fullScreen.js
+++ plugins/sigma.plugins.fullScreen/sigma.plugins.fullScreen.js
@@ -4,63 +4,80 @@
   if (typeof sigma === 'undefined')
     throw 'sigma is not declared';
 
-  // Initialize package:
-  sigma.utils.pkg('sigma.plugins');
-
   /**
    * Sigma Fullscreen
    * =============================
    *
    * @author Martin de la Taille <https://github.com/Martindelataille>
-   * @version 0.1
+   * @author Sébastien Heymann <https://github.com/sheymann>
+   * @version 0.2
    */
 
-  var _s = null,
-      _btn = null,
-      _renderer = null;
+  var _self = null,
+      _eventListenerElement = null;
 
-    // Use webkit or moz fullscreen method
-    function fullscreen(){
-         if(_renderer.container.webkitRequestFullScreen)
-             _renderer.container.webkitRequestFullScreen();
-        else
-           _renderer.container.mozRequestFullScreen();
+  function toggleFullScreen() {
+    if (!document.fullscreenElement &&    // alternative standard method
+        !document.mozFullScreenElement && !document.webkitFullscreenElement && !document.msFullscreenElement ) {  // current working methods
+      if (_self.container.requestFullscreen) {
+        _self.container.requestFullscreen();
+      } else if (_self.container.msRequestFullscreen) {
+        _self.container.msRequestFullscreen();
+      } else if (_self.container.mozRequestFullScreen) {
+        _self.container.mozRequestFullScreen();
+      } else if (_self.container.webkitRequestFullscreen) {
+        _self.container.webkitRequestFullscreen(Element.ALLOW_KEYBOARD_INPUT);
+      }
+    } else {
+      if (document.exitFullscreen) {
+        document.exitFullscreen();
+      } else if (document.msExitFullscreen) {
+        document.msExitFullscreen();
+      } else if (document.mozCancelFullScreen) {
+        document.mozCancelFullScreen();
+      } else if (document.webkitExitFullscreen) {
+        document.webkitExitFullscreen();
+      }
     }
+  }
 
   /**
-   * Interface
-   * ------------------
-   */
-
-  /**
-   * This plugin enables the activation of fullscreen by clicking on btn
-   * If btn not exist, this plugin will take graph as a btn
+   * This plugin enables the activation of full screen mode by clicking on btn.
+   * If btn does not exist, this plugin will leave the full screen mode.
    *
-   * @param  {sigma}                     s The related sigma instance.
-   * @object  {btn} a The btn id from the page.
+   * @param  {?object} btn The btn id from the page.
    */
-  sigma.plugins.fullScreen = function(s, btn) {
-    _s = s;
-    _renderer = s.renderers[0];
-    _btn = btn;
-
-    var eventListenerElement;
+  function fullScreen(btn) {
+    _self = this,
+    _eventListenerElement = null;
+    
     // Get the btn element reference from the DOM
-    if(_btn)
-      eventListenerElement = document.getElementById(_btn.id);
-    else
-      eventListenerElement = _renderer.contexts.mouse.canvas;
-
-    eventListenerElement.addEventListener("click",fullscreen);
+    if(btn) {
+      _eventListenerElement = document.getElementById(btn.id);
+      _eventListenerElement.removeEventListener("click", toggleFullScreen);
+      _eventListenerElement.addEventListener("click", toggleFullScreen);
+    } 
+    else {
+      toggleFullScreen();
+    }
   };
 
   /**
-   *  This function kills the fullscreen instance.
+   *  This function kills the fullScreen instance.
    */
-  sigma.plugins.killFullscreen = function() {
-    _s = null;
-    _btn = null;
-    _renderer = null;
+  function killFullScreen() {
+    toggleFullScreen();
+    _self = null;
+    
+    if (_eventListenerElement)
+      _eventListenerElement.removeEventListener("click", toggleFullScreen);
   };
+
+  // Extending canvas and webl renderers
+  sigma.renderers.canvas.prototype.fullScreen = fullScreen;
+  sigma.renderers.webgl.prototype.fullScreen = fullScreen;
+
+  sigma.renderers.canvas.prototype.killFullScreen = killFullScreen;
+  sigma.renderers.webgl.prototype.killFullScreen = killFullScreen;
 
 }).call(this);
