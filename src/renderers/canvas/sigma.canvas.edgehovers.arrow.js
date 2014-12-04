8b902f5d208fe94f159388378289e0af26d15819
likun <kunsland1990@gmail.com>
2014-12-04 18:13:04 +0800

    fix edgehover bug when hovering arrow type edges

--- src/renderers/canvas/sigma.canvas.edgehovers.arrow.js
+++ src/renderers/canvas/sigma.canvas.edgehovers.arrow.js
@@ -19,15 +19,13 @@
         edgeColor = settings('edgeColor'),
         defaultNodeColor = settings('defaultNodeColor'),
         defaultEdgeColor = settings('defaultEdgeColor'),
-        size = edge[prefix + 'size'] || 1,
+        size = settings('edgeHoverSizeRatio')*(edge[prefix + 'size'] || 1),
         tSize = target[prefix + 'size'],
         sX = source[prefix + 'x'],
         sY = source[prefix + 'y'],
         tX = target[prefix + 'x'],
         tY = target[prefix + 'y'];
 
-    size = (edge.hover) ?
-      settings('edgeHoverSizeRatio') * size : size;
     var aSize = size * 2.5,
         d = Math.sqrt(Math.pow(tX - sX, 2) + Math.pow(tY - sY, 2)),
         aX = sX + (tX - sX) * (d - aSize - tSize) / d,
