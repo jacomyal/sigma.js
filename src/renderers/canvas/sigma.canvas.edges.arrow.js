339ba3cf1ad41b8e2893314abfffaccc4a73b296
21stcaveman <zetta22000@yahoo.com>
2015-01-08 17:34:37 -0800

    First commit

--- src/renderers/canvas/sigma.canvas.edges.arrow.js
+++ src/renderers/canvas/sigma.canvas.edges.arrow.js
@@ -15,16 +15,19 @@
   sigma.canvas.edges.arrow = function(edge, source, target, context, settings) {
     var color = edge.color,
         prefix = settings('prefix') || '',
+        size = edge[prefix + 'size'] || 1,
+        overlay = edge['overlay'] || size+2,
+        percent = edge['percent'] || 0,
+	partial = !!edge['partial'],
         edgeColor = settings('edgeColor'),
         defaultNodeColor = settings('defaultNodeColor'),
         defaultEdgeColor = settings('defaultEdgeColor'),
-        size = edge[prefix + 'size'] || 1,
         tSize = target[prefix + 'size'],
         sX = source[prefix + 'x'],
         sY = source[prefix + 'y'],
         tX = target[prefix + 'x'],
         tY = target[prefix + 'y'],
-        aSize = Math.max(size * 2.5, settings('minArrowSize')),
+        aSize = Math.max(size * 4.5, settings('minArrowSize')),
         d = Math.sqrt(Math.pow(tX - sX, 2) + Math.pow(tY - sY, 2)),
         aX = sX + (tX - sX) * (d - aSize - tSize) / d,
         aY = sY + (tY - sY) * (d - aSize - tSize) / d,
@@ -44,23 +47,39 @@
           break;
       }
 
+    var dx = (aX - sX) / 100;
+    var dy = (aY - sY) / 100;
+
     context.strokeStyle = color;
-    context.lineWidth = size;
+    context.fillStyle = color;
+    if (!partial) {
+	context.lineWidth = size;
+	context.beginPath();
+	context.moveTo(sX, sY);
+	context.lineTo(aX,aY);
+	context.stroke();
+
+	context.beginPath();
+	context.moveTo(aX + vX, aY + vY);
+	context.lineTo(aX + vY * 0.6, aY - vX * 0.6);
+	context.lineTo(aX - vY * 0.6, aY + vX * 0.6);
+	context.lineTo(aX + vX, aY + vY);
+	context.closePath();
+	context.fill();
+    }
+    context.lineWidth = overlay;
     context.beginPath();
     context.moveTo(sX, sY);
-    context.lineTo(
-      aX,
-      aY
-    );
+    context.lineTo(sX + (dx * percent), sY + (dy * percent));
     context.stroke();
-
-    context.fillStyle = color;
+    
     context.beginPath();
-    context.moveTo(aX + vX, aY + vY);
-    context.lineTo(aX + vY * 0.6, aY - vX * 0.6);
-    context.lineTo(aX - vY * 0.6, aY + vX * 0.6);
-    context.lineTo(aX + vX, aY + vY);
+    context.moveTo(sX + (dx * percent) + vX, sY + (dy * percent) + vY);
+    context.lineTo(sX + (dx * percent) + vY * 0.6, sY + (dy * percent) - vX * 0.6);
+    context.lineTo(sX + (dx * percent) - vY * 0.6, sY + (dy * percent) + vX * 0.6);
+    context.lineTo(sX + vX + (dx * percent), sY + vY + (dy * percent));
     context.closePath();
     context.fill();
+
   };
 })();
