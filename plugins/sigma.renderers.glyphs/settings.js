6771f7c889d35acc5bc4a566524fbaeca8732ff8
Sébastien Heymann <seb@linkurio.us>
2015-01-09 11:46:40 +0100

    Refactor and optimize

--- plugins/sigma.renderers.glyphs/settings.js
+++ plugins/sigma.renderers.glyphs/settings.js
@@ -11,15 +11,27 @@
   * Extended sigma settings.
   */
   var settings = {
+    // {number} The glyph radius in pixels.
     glyphRadius: 10,
+    // {string} The glyph background-color.
     glyphFillColor: 'white',
+    // {string} The glyph text-color.
     glyphTextColor: 'black',
+    // {string} The glyph border-color.
     glyphStrokeColor: 'black',
+    // {number} The glyph border-width in pixels.
     glyphLineWidth: 2,
+    // {string} The glyph text font-style.
     glyphFontStyle: 'normal',
+    // {number} The glyph text font-size in pixels.
     glyphFontSize: 6,
-    glyphFontName: 'Arial',
-    glyphHidden: false
+    // {string} The glyph text font-family in pixels. Should be included if
+    // needed with @font-face or equivalent.
+    glyphFont: 'Arial',
+    // {number} The minimum size a node must have to see its glyph text displayed.
+    glyphTextThreshold: 6,
+    // {boolean} A flag to display glyphs or not.
+    drawGlyphs: true
   };
 
   // Export the previously designed settings:
--- plugins/sigma.renderers.glyphs/sigma.renderers.glyphs.js
+++ plugins/sigma.renderers.glyphs/sigma.renderers.glyphs.js
@@ -5,7 +5,8 @@
    * Sigma Renderer Glyphs Utility
    * ================================
    *
-   * The aim of this plugin is to display customized glyphs around a node, at four possible positions.
+   * The aim of this plugin is to display customized glyphs around a node,
+   * at four possible positions.
    *
    * Author: Florent Schildknecht (Flo-Schield-Bobby)
    * Version: 0.0.1
@@ -14,138 +15,129 @@
     throw 'sigma is not declared';
   }
 
+  // Utility function
+  function degreesToRadians (degrees) {
+    return degrees * Math.PI / 180;
+  }
+
   // Main method: create glyphs canvas and append it to the scene
   function glyphs (params) {
     params = params || {};
+
+    var defFont = params.font || this.settings('glyphFont'),
+        defFontStyle = params.fontStyle || this.settings('glyphFontStyle'),
+        defFontSize = this.settings('labelSizeRatio') *
+          (params.fontSize || this.settings('glyphFontSize')) *
+          this.settings('zoomingRatio'),
+        defStrokeColor = params.strokeColor || this.settings('glyphStrokeColor'),
+        defLineWidth = params.lineWidth || this.settings('glyphLineWidth'),
+        defFillColor = params.fillColor || this.settings('glyphFillColor'),
+        defRadius = params.radius || this.settings('glyphRadius'),
+        defTextColor = params.textColor || this.settings('glyphTextColor'),
+        defTextThreshold = params.textThreshold || this.settings('glyphTextThreshold'),
+        defDraw = params.draw || this.settings('drawGlyphs');
 
     if (!this.domElements['glyphs']) {
       this.initDOM('canvas', 'glyphs');
       this.domElements['glyphs'].width = this.container.offsetWidth;
       this.domElements['glyphs'].height = this.container.offsetHeight;
-      this.container.insertBefore(this.domElements['glyphs'], this.domElements['glyphs'].previousSibling);
-      this.drawingContext = this.domElements['glyphs'].getContext('2d');
+      this.container.insertBefore(
+        this.domElements['glyphs'],
+        this.domElements['glyphs'].previousSibling
+      );
     }
+    this.drawingContext = this.domElements['glyphs'].getContext('2d');
+    this.drawingContext.textAlign = 'center';
+    this.drawingContext.lineWidth = defLineWidth;
+    this.drawingContext.font = defFontStyle + ' ' + defFontSize + 'px ' + defFont;
+    this.drawingContext.strokeStyle = defStrokeColor;
 
-    var nodes = this.nodesOnScreen,
+
+    var self = this,
+        nodes = this.nodesOnScreen || [],
         prefix = this.options.prefix || '',
-        degreesToRadians = function (degrees) {
-          return degrees * Math.PI / 180;
-        },
-        drawGlyph = function (settings) {
-          if (settings.node && settings.node[prefix + 'x'] && settings.node[prefix + 'y']) {
-            var node = settings.node,
-                x = node[prefix + 'x'],
-                y = node[prefix + 'y'],
-                nodeRadius = node[prefix + 'size'],
-                glyphRadius = settings.size || this.settings('glyphRadius') || node[prefix + 'size'] / 4,
-                fillColor = settings.fillColor || this.settings('glyphFillColor'),
-                textColor = settings.textColor || this.settings('glyphTextColor'),
-                strokeColor = settings.strokeColor || this.settings('glyphStrokeColor'),
-                lineWidth = settings.lineWidth || this.settings('glyphLineWidth'),
-                fontStyle = settings.fontStyle || this.settings('glyphFontStyle'),
-                fontName = settings.fontName || this.settings('glyphFontName'),
-                fontSize = this.settings('labelSizeRatio') * (settings.fontSize || this.settings('glyphFontSize')),
-                hidden = settings.hidden || this.settings('glyphHidden') || false,
-                content = settings.content.toString() || '';
+        cos45 = Math.cos(degreesToRadians(45)),
+        sin45 = Math.sin(degreesToRadians(45)),
+        cos135 = Math.cos(degreesToRadians(135)),
+        sin135 = Math.sin(degreesToRadians(135)),
+        cos225 = Math.cos(degreesToRadians(225)),
+        sin225 = Math.sin(degreesToRadians(225)),
+        cos315 = Math.cos(degreesToRadians(315)),
+        sin315 = Math.sin(degreesToRadians(315));
 
-            if (hidden !== true) {
-              fontSize *= this.settings('zoomingRatio');
+    function draw (o, textThreshold, context) {
+      if (o.draw && o.x && o.y) {
+        var x = o.x,
+            y = o.y;
 
-              switch (settings.position) {
-                case 'top-right':
-                  x += nodeRadius * Math.cos(degreesToRadians(45));
-                  y += nodeRadius * Math.sin(degreesToRadians(45));
-                  break;
-                case 'top-left':
-                  x += nodeRadius * Math.cos(degreesToRadians(135));
-                  y += nodeRadius * Math.sin(degreesToRadians(135));
-                  break;
-                case 'bottom-left':
-                  x += nodeRadius * Math.cos(degreesToRadians(225));
-                  y += nodeRadius * Math.sin(degreesToRadians(225));
-                  break;
-                case 'bottom-right':
-                  x += nodeRadius * Math.cos(degreesToRadians(315));
-                  y += nodeRadius * Math.sin(degreesToRadians(315));
-                  break;
-              }
+        switch (o.position) {
+          case 'top-right':
+            x += o.nodeSize * cos45;
+            y += o.nodeSize * sin45;
+            break;
+          case 'top-left':
+            x += o.nodeSize * cos135;
+            y += o.nodeSize * sin135;
+            break;
+          case 'bottom-left':
+            x += o.nodeSize * cos225;
+            y += o.nodeSize * sin225;
+            break;
+          case 'bottom-right':
+            x += o.nodeSize * cos315;
+            y += o.nodeSize * sin315;
+            break;
+        }
 
-              // Glyph rendering
-              this.drawingContext.fillStyle = fillColor;
-              this.drawingContext.strokeStyle = strokeColor;
-              this.drawingContext.lineWidth = lineWidth;
-              this.drawingContext.beginPath();
-              this.drawingContext.arc(x, y, glyphRadius, 2 * Math.PI, false);
-              this.drawingContext.closePath();
-              this.drawingContext.stroke();
-              this.drawingContext.fill();
+        // Glyph rendering
+        context.fillStyle = o.fillColor;
+        if (o.strokeColor !== context.strokeStyle) {
+          context.strokeStyle = o.strokeColor;
+        }
+        context.beginPath();
+        context.arc(x, y, o.radius, 2 * Math.PI, false);
+        context.closePath();
+        if (context.lineWidth) context.stroke();
+        context.fill();
 
-              // Glyph content rendering
-              this.drawingContext.font = fontStyle + ' ' + fontSize + 'px ' + fontName;
-              var textWidth = this.drawingContext.measureText(content).width;
-
-              if ((textWidth <= nodeRadius) && (textWidth >= this.settings('labelThreshold'))) {
-                this.drawingContext.fillStyle = textColor;
-                this.drawingContext.textAlign = 'center';
-                this.drawingContext.fillText(content, x, y + 2);
-              }
-            }
+        // Glyph content rendering
+        if (o.radius > textThreshold) {
+          var font =  o.fontStyle + ' ' + o.fontSize + 'px ' + o.font;
+          if (font !== context.font) {
+            context.font = font;
           }
-        };
-
-
-    // Get the glyphs definitions from parameters
-    if (params.glyphs && params.glyphs.length > 0) {
-      var glyphsLength = params.glyphs.length;
-      while (glyphsLength--) {
-        var glyph = params.glyphs[glyphsLength];
-
-        if (glyph.nodeId && glyph.position) {
-          drawGlyph.apply(this, [{
-            node: this.graph.nodes(glyph.nodeId),
-            position: glyph.position,
-            size: glyph.size || node[prefix + 'size'] / 2,
-            content: glyph.content,
-            fillColor: glyph.fillColor,
-            textColor: glyph.textColor,
-            strokeColor: glyph.strokeColor,
-            lineWidth: glyph.lineWidth,
-            fontStyle: glyph.fontStyle,
-            fontName: glyph.fontName,
-            fontSize: glyph.fontSize,
-            hidden: glyph.hidden
-          }]);
+          context.fillStyle = o.textColor;
+          context.fillText(o.content, x, y + o.radius * 0.5);
         }
       }
-    } else {
-      // Try to get them from node attributes
-      var nodesLength = nodes.length;
-      while (nodesLength--) {
-        var node = nodes[nodesLength];
+    };
 
-        if (node.glyphs) {
-          var glyphsLength = node.glyphs.length;
-          while (glyphsLength--) {
-            var glyph = node.glyphs[glyphsLength];
-
-            drawGlyph.apply(this, [{
-              node: node,
+    nodes.forEach(function(node) {
+      if (node.glyphs) {
+        node.glyphs.forEach(function(glyph) {
+          draw(
+            {
+              x: node[prefix + 'x'],
+              y: node[prefix + 'y'],
+              nodeSize: node[prefix + 'size'] || 0,
               position: glyph.position,
-              size: glyph.size || node[prefix + 'size'] / 2,
-              content: glyph.content,
-              fillColor: glyph.fillColor,
-              textColor: glyph.textColor,
-              strokeColor: glyph.strokeColor,
-              lineWidth: glyph.lineWidth,
-              fontStyle: glyph.fontStyle,
-              fontName: glyph.fontName,
-              fontSize: glyph.fontSize,
-              hidden: glyph.hidden
-            }]);
-          }
-        }
+              radius: glyph.size || node[prefix + 'size'] / 2 || defRadius,
+              content: (glyph.content || '').toString() || '',
+              lineWidth: glyph.lineWidth || defLineWidth,
+              fillColor: glyph.fillColor || defFillColor,
+              textColor: glyph.textColor || defTextColor,
+              strokeColor: glyph.strokeColor || defStrokeColor,
+              fontStyle: glyph.fontStyle || defFontStyle,
+              font: glyph.font || defFont,
+              fontSize: glyph.fontSize || defFontSize,
+              draw: !glyph.hidden || defDraw
+            },
+            glyph.threshold || defTextThreshold,
+            self.drawingContext
+          );
+        });
       }
-    }
+    });
   }
 
   // Bind glyphs method to renderers
