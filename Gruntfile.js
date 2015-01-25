37389aff0c6ea1481b0d055364dadd126697f992
Sébastien Heymann <seb@linkurio.us>
2015-01-25 16:13:11 +0000

    Remove sigma.plugin.designer

--- Gruntfile.js
+++ Gruntfile.js
@@ -80,7 +80,6 @@ module.exports = function(grunt) {
     'plugins.activeState',
     'plugins.animate',
     'plugins.colorbrewer',
-    'plugins.designer',
     'plugins.dragNodes',
     'plugins.edgeSiblings',
     'plugins.image',
