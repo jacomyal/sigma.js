aacaa1c7bcb31a09659294797ea49ab5c188bd2e
Sébastien Heymann <seb@linkurio.us>
2015-02-04 11:53:37 +0100

    Add label alignment to renderers.linkurious

--- plugins/sigma.renderers.linkurious/canvas/sigma.canvas.hovers.def.js
+++ plugins/sigma.renderers.linkurious/canvas/sigma.canvas.hovers.def.js
@@ -25,50 +25,15 @@
         size = node[prefix + 'size'] || 1,
         defaultNodeColor = settings('defaultNodeColor'),
         borderSize = settings('borderSize'),
+        alignment = settings('labelAlignment'),
         fontSize = (settings('labelSize') === 'fixed') ?
           settings('defaultLabelSize') :
           settings('labelSizeRatio') * size,
         color = node.color || defaultNodeColor;
 
-    // Label background:
-    context.font = (fontStyle ? fontStyle + ' ' : '') +
-      fontSize + 'px ' + (settings('hoverFont') || settings('font'));
-
-    context.beginPath();
-    context.fillStyle = settings('labelHoverBGColor') === 'node' ?
-      (node.color || settings('defaultNodeColor')) :
-      settings('defaultHoverLabelBGColor');
-
-    if (node.label && settings('labelHoverShadow')) {
-      context.shadowOffsetX = 0;
-      context.shadowOffsetY = 0;
-      context.shadowBlur = 8;
-      context.shadowColor = settings('labelHoverShadowColor');
-    }
-
-    if (node.label && typeof node.label === 'string') {
-      x = Math.round(node[prefix + 'x'] - fontSize / 2 - 2);
-      y = Math.round(node[prefix + 'y'] - fontSize / 2 - 2);
-      w = Math.round(
-        context.measureText(node.label).width + fontSize / 2 + size + 10
-      );
-      h = Math.round(fontSize + 4);
-      e = Math.round(fontSize / 2 + 2);
-
-      context.moveTo(x, y + e);
-      context.arcTo(x, y, x + e, y, e);
-      context.lineTo(x + w, y);
-      context.lineTo(x + w, y + h);
-      context.lineTo(x + e, y + h);
-      context.arcTo(x, y + h, x, y + h - e, e);
-      context.lineTo(x, y + e);
-
-      context.closePath();
-      context.fill();
-
-      context.shadowOffsetX = 0;
-      context.shadowOffsetY = 0;
-      context.shadowBlur = 0;
+    if (alignment !== 'center') {
+      prepareLabelBackground(context);
+      drawHoverBorder(alignment, context, fontSize, node);
     }
 
     // Node border:
@@ -93,21 +58,133 @@
     var nodeRenderer = sigma.canvas.nodes[node.type] || sigma.canvas.nodes.def;
     nodeRenderer(node, context, settings);
 
+    if (alignment === 'center') {
+      prepareLabelBackground(context);
+      drawHoverBorder(alignment, context, fontSize, node);
+    }
+
     // Display the label:
     if (typeof node.label === 'string') {
       context.fillStyle = (settings('labelHoverColor') === 'node') ?
         (node.color || settings('defaultNodeColor')) :
         settings('defaultLabelHoverColor');
 
+      var labelWidth = context.measureText(node.label).width,
+          labelOffsetX = - labelWidth / 2,
+          labelOffsetY = fontSize / 3;
+
+      switch (alignment) {
+        case 'bottom':
+          labelOffsetY = + size + 4 * fontSize / 3;
+          break;
+        case 'center':
+          break;
+        case 'left':
+          labelOffsetX = - size - borderSize - settings('outerBorderSize') - 3 - labelWidth;
+          break;
+        case 'top':
+          labelOffsetY = - size - 2 * fontSize / 3;
+          break;
+        case 'inside':
+          if (labelWidth <= (size + fontSize / 3) * 2) {
+            break;
+          }
+        /* falls through*/
+        case 'right':
+        /* falls through*/
+        default:
+          labelOffsetX = size + borderSize + settings('outerBorderSize') + 3;
+          break;
+      }
+
       context.fillText(
         node.label,
-        Math.round(node[prefix + 'x'] +
-          size +
-          borderSize +
-          settings('outerBorderSize') +
-          3),
-        Math.round(node[prefix + 'y'] + fontSize / 3)
+        Math.round(node[prefix + 'x'] + labelOffsetX),
+        Math.round(node[prefix + 'y'] + labelOffsetY)
       );
+    }
+
+    function prepareLabelBackground(context) {
+      context.font = (fontStyle ? fontStyle + ' ' : '') +
+        fontSize + 'px ' + (settings('hoverFont') || settings('font'));
+
+      context.beginPath();
+      context.fillStyle = settings('labelHoverBGColor') === 'node' ?
+        (node.color || settings('defaultNodeColor')) :
+        settings('defaultHoverLabelBGColor');
+
+      if (node.label && settings('labelHoverShadow')) {
+        context.shadowOffsetX = 0;
+        context.shadowOffsetY = 0;
+        context.shadowBlur = 8;
+        context.shadowColor = settings('labelHoverShadowColor');
+      }
+    }
+
+    function drawHoverBorder(alignment, context, fontSize, node) {
+      var x = Math.round(node[prefix + 'x']),
+          y = Math.round(node[prefix + 'y']),
+          w = Math.round(
+            context.measureText(node.label).width + size + 1.5 + fontSize / 3
+          ),
+          h = fontSize + 4,
+          e = Math.round(size + fontSize / 4);
+
+      if (node.label && typeof node.label === 'string') {
+        // draw a rectangle for the label
+        switch (alignment) {
+          case 'center':
+            y = Math.round(node[prefix + 'y'] - fontSize / 2 - 2);
+            context.rect(x - w / 2, y, w, h);
+            break;
+          case 'left':
+            x = Math.round(node[prefix + 'x'] + fontSize / 2 + 2);
+            y = Math.round(node[prefix + 'y'] - fontSize / 2 - 2);
+
+            context.moveTo(x, y + e);
+            context.arcTo(x, y, x - e, y, e);
+            context.lineTo(x - w - borderSize - e, y);
+            context.lineTo(x - w - borderSize - e, y + h);
+            context.lineTo(x - e, y + h);
+            context.arcTo(x, y + h, x, y + h - e, e);
+            context.lineTo(x, y + e);
+            break;
+          case 'top':
+            context.rect(x - w / 2, y - e - h, w, h);
+            break;
+          case 'bottom':
+            context.rect(x - w / 2, y + e, w, h);
+            break;
+          case 'inside':
+            if (context.measureText(node.label).width <= e * 2) {
+              // don't draw anything
+              break;
+            }
+            // use default setting, falling through
+          /* falls through*/
+          case 'right':
+          /* falls through*/
+          default:
+            x = Math.round(node[prefix + 'x'] - fontSize / 2 - 2);
+            y = Math.round(node[prefix + 'y'] - fontSize / 2 - 2);
+
+            context.moveTo(x, y + e);
+            context.arcTo(x, y, x + e, y, e);
+            context.lineTo(x + w + borderSize + e, y);
+            context.lineTo(x + w + borderSize + e, y + h);
+            context.lineTo(x + e, y + h);
+            context.arcTo(x, y + h, x, y + h - e, e);
+            context.lineTo(x, y + e);
+            break;
+        }
+      }
+
+      context.closePath();
+      context.fill();
+
+      context.shadowOffsetX = 0;
+      context.shadowOffsetY = 0;
+      context.shadowBlur = 0;
     }
   };
 }).call(this);
