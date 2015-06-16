4abd97daa4907ac1e86c22dfc47aba4dd15923ef
Sébastien Heymann <seb@linkurio.us>
2015-06-16 11:51:40 +0200

    Fix #125 Add edge label hovering effects

--- plugins/sigma.renderers.edgeLabels/sigma.canvas.edges.labels.def.js
+++ plugins/sigma.renderers.edgeLabels/sigma.canvas.edges.labels.def.js
@@ -2,7 +2,7 @@
   'use strict';
 
   if (typeof sigma === 'undefined')
-    throw new Error('sigma is not declared');
+    throw 'sigma is not declared';
 
   // Initialize packages:
   sigma.utils.pkg('sigma.canvas.edges.labels');
@@ -26,19 +26,23 @@
     var prefix = settings('prefix') || '',
         size = edge[prefix + 'size'] || 1;
 
-    if (size < settings('edgeLabelThreshold'))
+    if (size < settings('edgeLabelThreshold') && !edge.hover)
       return;
 
     if (0 === settings('edgeLabelSizePowRatio'))
-      throw new Error('Invalid setting: "edgeLabelSizePowRatio" is equal to 0.');
+      throw '"edgeLabelSizePowRatio" must not be 0.';
 
     var fontSize,
         angle = 0,
+        fontStyle = edge.hover ?
+          (settings('hoverFontStyle') || settings('fontStyle')) :
+          settings('fontStyle'),
         x = (source[prefix + 'x'] + target[prefix + 'x']) / 2,
         y = (source[prefix + 'y'] + target[prefix + 'y']) / 2,
         dX = target[prefix + 'x'] - source[prefix + 'x'],
         dY = target[prefix + 'y'] - source[prefix + 'y'],
-        sign = (source[prefix + 'x'] < target[prefix + 'x']) ? 1 : -1;
+        sign = (source[prefix + 'x'] < target[prefix + 'x']) ? 1 : -1,
+        angle = Math.atan2(dY * sign, dX * sign);
 
     // The font size is sublineraly proportional to the edge size, in order to
     // avoid very large labels on screen.
@@ -57,27 +61,17 @@
 
     if (edge.active) {
       context.font = [
-        settings('activeFontStyle'),
+        settings('activeFontStyle') || settings('fontStyle'),
         fontSize + 'px',
         settings('activeFont') || settings('font')
       ].join(' ');
-
-      context.fillStyle =
-        settings('edgeActiveColor') === 'edge' ?
-        (edge.active_color || settings('defaultEdgeActiveColor')) :
-        settings('defaultEdgeLabelActiveColor');
     }
     else {
       context.font = [
-        settings('fontStyle'),
+        fontStyle,
         fontSize + 'px',
         settings('font')
       ].join(' ');
-
-      context.fillStyle =
-        (settings('edgeLabelColor') === 'edge') ?
-        (edge.color || settings('defaultEdgeColor')) :
-        settings('defaultEdgeLabelColor');
     }
 
     context.textAlign = 'center';
@@ -102,6 +96,40 @@
       }
     }
 
+    if (edge.hover) {
+      // Label background:
+      context.fillStyle = settings('edgeLabelHoverBGColor') === 'edge' ?
+        (edge.color || settings('defaultEdgeColor')) :
+        settings('defaultEdgeHoverLabelBGColor');
+
+      if (settings('edgeLabelHoverShadow')) {
+        context.shadowOffsetX = 0;
+        context.shadowOffsetY = 0;
+        context.shadowBlur = 8;
+        context.shadowColor = settings('edgeLabelHoverShadowColor');
+      }
+
+      drawBackground(angle, context, fontSize, size, edge.label, x, y);
+
+      if (settings('edgeLabelHoverShadow')) {
+        context.shadowBlur = 0;
+        context.shadowColor = '#000';
+      }
+    }
+
+    if (edge.active) {
+      context.fillStyle =
+        settings('edgeActiveColor') === 'edge' ?
+        (edge.active_color || settings('defaultEdgeActiveColor')) :
+        settings('defaultEdgeLabelActiveColor');
+    }
+    else {
+      context.fillStyle =
+        (settings('edgeLabelColor') === 'edge') ?
+        (edge.color || settings('defaultEdgeColor')) :
+        settings('defaultEdgeLabelColor');
+    }
+
     context.translate(x, y);
     context.rotate(angle);
     context.fillText(
@@ -111,5 +139,24 @@
     );
 
     context.restore();
+
+    function drawBackground(angle, context, fontSize, size, label, x, y) {
+      var w = Math.round(
+            context.measureText(label).width + size + 1.5 + fontSize / 3
+          ),
+          h = fontSize + 4;
+
+      context.save();
+      context.beginPath();
+
+      // draw a rectangle for the label
+      context.translate(x, y);
+      context.rotate(angle);
+      context.rect(-w / 2, -h -size/2, w, h);
+
+      context.closePath();
+      context.fill();
+      context.restore();
+    }
   };
 }).call(this);
