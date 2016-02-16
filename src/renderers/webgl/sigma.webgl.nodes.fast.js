5752fe9951195ec4828f58e69e48db8449822fe2
Yomguithereal <guillaumeplique@gmail.com>
2016-02-16 22:08:51 +0100

    Attempting to display nodes with gl.POINTS and anti-alias

--- src/renderers/webgl/sigma.webgl.nodes.fast.js
+++ src/renderers/webgl/sigma.webgl.nodes.fast.js
@@ -149,7 +149,20 @@
           'varying vec4 color;',
 
           'void main(void) {',
-            'gl_FragColor = color;',
+            'float border = 0.01;',
+            'float radius = 0.5;',
+
+            'vec4 color0 = vec4(0.0, 0.0, 0.0, 0.0);',
+            'vec2 m = gl_PointCoord - vec2(0.5, 0.5);',
+            'float dist = radius - sqrt(m.x * m.x + m.y * m.y);',
+
+            'float t = 0.0;',
+            'if (dist > border)',
+              't = 1.0;',
+            'else if (dist > 0.0)',
+              't = dist / border;',
+
+            'gl_FragColor = mix(color0, color, t);',
           '}'
         ].join('\n'),
         gl.FRAGMENT_SHADER
