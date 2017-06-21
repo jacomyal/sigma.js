1e3081fdd311fe3503f6b92fbd6e92886a4f94f6
Alexis Jacomy <alexis.jacomy@gmail.com>
2017-06-21 09:13:03 +0200

    Anti-aliased nodes rendering frag shader

--- src/renderers/webgl/shaders/node.frag.glsl
+++ src/renderers/webgl/shaders/node.frag.glsl
@@ -5,14 +5,17 @@ varying vec2 center;
 varying float radius;
 
 void main(void) {
+  float border_size = 0.8;
   vec4 color0 = vec4(0.0, 0.0, 0.0, 0.0);
 
   vec2 m = gl_FragCoord.xy - center;
-  float diff = radius - sqrt(m.x * m.x + m.y * m.y);
+  float dist = sqrt(m.x * m.x + m.y * m.y);
+  float t = smoothstep(
+    radius + border_size,
+    radius - border_size,
+    dist
+  );
 
   // Here is how we draw a disc instead of a square:
-  if (diff > 0.0)
-    gl_FragColor = color;
-  else
-    gl_FragColor = color0;
+  gl_FragColor = mix(color0, color, t);
 }
