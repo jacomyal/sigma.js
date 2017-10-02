04f0df8131c9da15eef8fac1ec4dd06df69f9897
Yomguithereal <guillaumeplique@gmail.com>
2017-10-02 17:30:34 +0200

    Fixing minification

--- examples/webpack.config.js
+++ examples/webpack.config.js
@@ -66,7 +66,7 @@ module.exports = {
     rules: [
       {
         test: /\.js$/,
-        exclude: /node_modules/,
+        exclude: /node_modules(?!\/gl-matrix)/,
         loader: 'babel-loader'
       },
       {
--- webpack.config.js
+++ webpack.config.js
@@ -21,7 +21,7 @@ module.exports = {
       },
       {
         test: /\.js$/,
-        exclude: /node_modules/,
+        exclude: /node_modules(?!\/gl-matrix)/,
         loader: 'babel-loader'
       }
     ]
