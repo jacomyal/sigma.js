3b4687812e8b2952835443d7d4d67ed9c86f3ff9
Yomguithereal <guillaumeplique@gmail.com>
2017-10-12 12:01:32 +0200

    Better node.fast program lifecycle

--- src/renderers/webgl/programs/node.fast.js
+++ src/renderers/webgl/programs/node.fast.js
@@ -14,6 +14,51 @@ import fragmentShaderSource from '../shaders/node.fast.frag.glsl';
 export default class NodeProgramFast extends Program {
   constructor(gl) {
     super(gl, vertexShaderSource, fragmentShaderSource);
+
+    // Initializing buffers
+    this.buffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, this.buffer);
+
+    const program = this.program;
+
+    // Locations
+    this.positionLocation = gl.getAttribLocation(program, 'a_position'),
+    this.sizeLocation = gl.getAttribLocation(program, 'a_size'),
+    this.colorLocation = gl.getAttribLocation(program, 'a_color'),
+    this.resolutionLocation = gl.getUniformLocation(program, 'u_resolution'),
+    this.matrixLocation = gl.getUniformLocation(program, 'u_matrix'),
+    this.ratioLocation = gl.getUniformLocation(program, 'u_ratio'),
+    this.scaleLocation = gl.getUniformLocation(program, 'u_scale');
+
+    // Bindings
+    gl.enableVertexAttribArray(this.positionLocation);
+    gl.enableVertexAttribArray(this.sizeLocation);
+    gl.enableVertexAttribArray(this.colorLocation);
+
+    gl.vertexAttribPointer(
+      this.positionLocation,
+      2,
+      gl.FLOAT,
+      false,
+      NodeProgramFast.ATTRIBUTES * Float32Array.BYTES_PER_ELEMENT,
+      0
+    );
+    gl.vertexAttribPointer(
+      this.sizeLocation,
+      1,
+      gl.FLOAT,
+      false,
+      NodeProgramFast.ATTRIBUTES * Float32Array.BYTES_PER_ELEMENT,
+      8
+    );
+    gl.vertexAttribPointer(
+      this.colorLocation,
+      1,
+      gl.FLOAT,
+      false,
+      NodeProgramFast.ATTRIBUTES * Float32Array.BYTES_PER_ELEMENT,
+      12
+    );
   }
 
   process(array, data, i) {
@@ -29,55 +74,15 @@ export default class NodeProgramFast extends Program {
     const program = this.program;
     gl.useProgram(program);
 
-    // Attribute locations
-    const positionLocation = gl.getAttribLocation(program, 'a_position'),
-          sizeLocation = gl.getAttribLocation(program, 'a_size'),
-          colorLocation = gl.getAttribLocation(program, 'a_color'),
-          resolutionLocation = gl.getUniformLocation(program, 'u_resolution'),
-          matrixLocation = gl.getUniformLocation(program, 'u_matrix'),
-          ratioLocation = gl.getUniformLocation(program, 'u_ratio'),
-          scaleLocation = gl.getUniformLocation(program, 'u_scale');
-
-    const buffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
     gl.bufferData(gl.ARRAY_BUFFER, array, gl.DYNAMIC_DRAW);
 
-    gl.uniform2f(resolutionLocation, params.width, params.height);
+    gl.uniform2f(this.resolutionLocation, params.width, params.height);
     gl.uniform1f(
-      ratioLocation,
+      this.ratioLocation,
       1 / Math.pow(params.ratio, params.nodesPowRatio)
     );
-    gl.uniform1f(scaleLocation, params.scalingRatio);
-    gl.uniformMatrix3fv(matrixLocation, false, params.matrix);
-
-    gl.enableVertexAttribArray(positionLocation);
-    gl.enableVertexAttribArray(sizeLocation);
-    gl.enableVertexAttribArray(colorLocation);
-
-    gl.vertexAttribPointer(
-      positionLocation,
-      2,
-      gl.FLOAT,
-      false,
-      NodeProgramFast.ATTRIBUTES * Float32Array.BYTES_PER_ELEMENT,
-      0
-    );
-    gl.vertexAttribPointer(
-      sizeLocation,
-      1,
-      gl.FLOAT,
-      false,
-      NodeProgramFast.ATTRIBUTES * Float32Array.BYTES_PER_ELEMENT,
-      8
-    );
-    gl.vertexAttribPointer(
-      colorLocation,
-      1,
-      gl.FLOAT,
-      false,
-      NodeProgramFast.ATTRIBUTES * Float32Array.BYTES_PER_ELEMENT,
-      12
-    );
+    gl.uniform1f(this.scaleLocation, params.scalingRatio);
+    gl.uniformMatrix3fv(this.matrixLocation, false, params.matrix);
 
     gl.drawArrays(
       gl.POINTS,
