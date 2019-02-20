ce5e3fc47448348f128c6d93ab8b601c6f523f9e
Chris Trevino <darthtrevino@gmail.com>
2019-02-19 16:21:08 -0800

    remove a couple of linting errors

--- src/sigma.export.js
+++ src/sigma.export.js
@@ -1,12 +1,9 @@
 // Hardcoded export for the node.js version:
-const sigma = this.sigma;
-
-const conrad = this.conrad;
-
+const { sigma, conrad } = this;
 sigma.conrad = conrad;
 
 // Dirty polyfills to permit sigma usage in node
-if (typeof HTMLElement === "undefined") HTMLElement = function() {};
+if (typeof HTMLElement === "undefined") HTMLElement = function noop() {};
 
 if (typeof window === "undefined")
   window = {
