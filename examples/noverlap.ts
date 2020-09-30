92a0e4d4c9244165c11a397c7be9eae1826a6dd0
Yomguithereal <guillaumeplique@gmail.com>
2020-09-30 22:33:34 +0200

    Better noverlap example

--- examples/noverlap.ts
+++ examples/noverlap.ts
@@ -4,13 +4,25 @@ import empty from "graphology-generators/classic/empty";
 import WebGLRenderer from "../src/renderers/webgl/index";
 import { animateNodes } from "../src/animate";
 import noverlap from "graphology-layout-noverlap";
+import NoverlapLayoutSupervisor from "graphology-layout-noverlap/worker";
 import chroma from "chroma-js";
 import random from "pandemonium/random";
 
+// import arctic from "./resources/arctic.gexf";
+// import gexf from "graphology-gexf/browser";
+
+const NOVERLAP_SETTINGS = {
+  margin: 2,
+  ratio: 1,
+  speed: 3,
+};
+
 const container = document.getElementById("container");
 
-const graph = empty(UndirectedGraph, 500);
+const graph = empty(UndirectedGraph, 1000);
 randomLayout.assign(graph);
+
+// const graph = gexf.parse(UndirectedGraph, arctic);
 
 graph.forEachNode((node) => {
   graph.mergeNodeAttributes(node, {
@@ -23,31 +35,60 @@ graph.forEachNode((node) => {
 const renderer = new WebGLRenderer(graph, container);
 const camera = renderer.getCamera();
 
-const button = document.createElement("button");
-button.textContent = "noverlap";
-button.style.position = "absolute";
-button.style.left = "10px";
-button.style.top = "10px";
+function createButton(text, offset) {
+  const button = document.createElement("button");
+  button.textContent = text;
+  button.style.position = "absolute";
+  button.style.left = "10px";
 
-container.appendChild(button);
+  let top = 10;
 
-// NOTE: sizeRatio is only relevant if you apply the algorithm while zooming or unzooming
-const reducer = (sizeRatio) => (key, attr) => {
-  const pos = camera.graphToViewport(renderer, attr.x, attr.y);
+  top += offset * 20;
+
+  button.style.top = `${top}px`;
+
+  container.appendChild(button);
+
+  return button;
+}
+
+const inputReducer = (key, attr) => {
+  let pos = renderer.normalizationFunction(attr);
+  pos = camera.graphToViewport(renderer, pos.x, pos.y);
 
   return {
     x: pos.x,
     y: pos.y,
-    size: attr.size / sizeRatio,
+    size: attr.size,
   };
 };
 
-button.onclick = () => {
-  var layout = noverlap(graph, { reducer: reducer(Math.pow(camera.getState().ratio, 0.5)), maxIterations: 500 });
+const outputReducer = (key, pos) => {
+  return renderer.normalizationFunction.inverse(camera.viewportToGraph(renderer, pos.x, pos.y));
+};
 
-  for (const node in layout) layout[node] = camera.viewportToGraph(renderer, layout[node].x, layout[node].y);
+const fixedButton = createButton("noverlap 500", 0);
+
+fixedButton.onclick = () => {
+  console.time("noverlap");
+  var layout = noverlap(graph, { inputReducer, outputReducer, maxIterations: 500, settings: NOVERLAP_SETTINGS });
+  console.timeEnd("noverlap");
 
   animateNodes(graph, layout, { duration: 100, easing: "linear" }, () => console.log("done"));
+};
+
+const supervisor = new NoverlapLayoutSupervisor(graph, { inputReducer, outputReducer, settings: NOVERLAP_SETTINGS });
+
+const startButton = createButton("start", 1);
+const stopButton = createButton("stop", 2);
+
+startButton.onclick = () => {
+  supervisor.start();
+};
+
+stopButton.onclick = () => {
+  supervisor.stop();
+  console.log(supervisor.converged);
 };
 
 window.renderer = renderer;
