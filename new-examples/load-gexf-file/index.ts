f81fdbf996895f8fb6cff447fe5d17e7aab01656
Alexis Jacomy <alexis.jacomy@gmail.com>
2021-10-05 16:40:41 +0200

    Improves load-gexf-file example

--- new-examples/load-gexf-file/index.ts
+++ new-examples/load-gexf-file/index.ts
@@ -2,40 +2,40 @@ import Sigma from "sigma";
 import Graph from "graphology";
 import { parse } from "graphology-gexf/browser";
 
-function init(graph: Graph) {
-  const container = document.getElementById("sigma-container") as HTMLElement;
-  const controls = document.getElementById("controls") as HTMLElement;
-
-  const renderer = new Sigma(graph, container);
-  const camera = renderer.getCamera();
-
-  // Bind interactions:
-  const zoomInBtn = controls.querySelector("#zoom-in") as HTMLButtonElement;
-  const zoomOutBtn = controls.querySelector("#zoom-out") as HTMLButtonElement;
-  const zoomResetBtn = controls.querySelector("#zoom-reset") as HTMLButtonElement;
-  const labelsThresholdRange = controls.querySelector("#labels-density") as HTMLInputElement;
-
-  // Bind zoom manipulation buttons
-  zoomInBtn.addEventListener("click", () => {
-    camera.animatedZoom({ duration: 600 });
-  });
-  zoomOutBtn.addEventListener("click", () => {
-    camera.animatedUnzoom({ duration: 600 });
-  });
-  zoomResetBtn.addEventListener("click", () => {
-    camera.animatedReset({ duration: 600 });
-  });
-
-  // Bind labels threshold to range input
-  labelsThresholdRange.addEventListener("change", () => {
-    renderer.setSetting("labelRenderedSizeThreshold", +labelsThresholdRange.value);
-  });
-
-  // Set proper range initial value:
-  labelsThresholdRange.value = renderer.getSetting("labelRenderedSizeThreshold") + "";
-}
-
 // Load external GEXF file:
 fetch("./public/arctic.gexf")
   .then((res) => res.text())
-  .then((xml) => init(parse(Graph, xml)));
+  .then((gexf) => {
+    // Parse GEXF string:
+    const graph = parse(Graph, gexf);
+
+    // Retrieve some useful DOM elements:
+    const container = document.getElementById("sigma-container") as HTMLElement;
+    const zoomInBtn = document.getElementById("zoom-in") as HTMLButtonElement;
+    const zoomOutBtn = document.getElementById("zoom-out") as HTMLButtonElement;
+    const zoomResetBtn = document.getElementById("zoom-reset") as HTMLButtonElement;
+    const labelsThresholdRange = document.getElementById("labels-density") as HTMLInputElement;
+
+    // Instanciate sigma:
+    const renderer = new Sigma(graph, container);
+    const camera = renderer.getCamera();
+
+    // Bind zoom manipulation buttons
+    zoomInBtn.addEventListener("click", () => {
+      camera.animatedZoom({ duration: 600 });
+    });
+    zoomOutBtn.addEventListener("click", () => {
+      camera.animatedUnzoom({ duration: 600 });
+    });
+    zoomResetBtn.addEventListener("click", () => {
+      camera.animatedReset({ duration: 600 });
+    });
+
+    // Bind labels threshold to range input
+    labelsThresholdRange.addEventListener("change", () => {
+      renderer.setSetting("labelRenderedSizeThreshold", +labelsThresholdRange.value);
+    });
+
+    // Set proper range initial value:
+    labelsThresholdRange.value = renderer.getSetting("labelRenderedSizeThreshold") + "";
+  });
