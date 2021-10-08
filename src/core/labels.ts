5ffa614a5f7182f8d1def0952be33985a1026220
Yomguithereal <guillaumeplique@gmail.com>
2021-10-08 16:36:41 +0200

    Dropping more dead code

--- src/core/labels.ts
+++ src/core/labels.ts
@@ -10,24 +10,6 @@ import { EdgeKey, NodeKey } from "graphology-types";
 
 import { Dimensions, Coordinates } from "../types";
 
-// TODO: full jsdocs
-
-/**
- * Helpers.
- */
-export function axisAlignedRectangularCollision(
-  x1: number,
-  y1: number,
-  w1: number,
-  h1: number,
-  x2: number,
-  y2: number,
-  w2: number,
-  h2: number,
-): boolean {
-  return x1 < x2 + w2 && x1 + w1 > x2 && y1 < y2 + h2 && y1 + h1 > y2;
-}
-
 /**
  * Classes.
  */
