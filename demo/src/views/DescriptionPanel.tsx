863cf396c80563abb1b9183ae50c83d3d231f956
Alexis Jacomy <alexis.jacomy@gmail.com>
2021-10-18 21:51:43 +0200

    [demo] Updates dataset
    Details:
    - Adds more tags (thanks @paulgirard)
    - Adds betweenness centrality to each node
    - Uses this new score as node sizes
    - Updates description to fit this update
    - Also swaps description links as _blank

--- demo/src/views/DescriptionPanel.tsx
+++ demo/src/views/DescriptionPanel.tsx
@@ -16,24 +16,52 @@ const DescriptionPanel: FC = () => {
       <p>
         This map represents a <i>network</i> of Wikipedia articles around the topic of "Data vizualisation". Each{" "}
         <i>node</i> represents an article, and each edge a{" "}
-        <a href="https://en.wikipedia.org/wiki/See_also">"See also" link</a>.
+        <a target="_blank" rel="noreferrer" href="https://en.wikipedia.org/wiki/See_also">
+          "See also" link
+        </a>
+        .
       </p>
       <p>
         The seed articles were selected by hand by the{" "}
-        <a href="https://medialab.sciencespo.fr/">Sciences-Po médialab</a> team, and the network was crawled using{" "}
-        <a href="https://densitydesign.github.io/strumentalia-seealsology/">Seealsology</a>, and then cleaned and
-        enriched manually. This makes the dataset creditable to both the médialab team and{" "}
-        <a href="https://en.wikipedia.org/wiki/Wikipedia:Wikipedians">Wikipedia editors</a>.
+        <a target="_blank" rel="noreferrer" href="https://medialab.sciencespo.fr/">
+          Sciences-Po médialab
+        </a>{" "}
+        team, and the network was crawled using{" "}
+        <a target="_blank" rel="noreferrer" href="https://densitydesign.github.io/strumentalia-seealsology/">
+          Seealsology
+        </a>
+        , and then cleaned and enriched manually. This makes the dataset creditable to both the médialab team and{" "}
+        <a target="_blank" rel="noreferrer" href="https://en.wikipedia.org/wiki/Wikipedia:Wikipedians">
+          Wikipedia editors
+        </a>
+        .
       </p>
       <p>
-        This web application has been developed by <a href="https://www.ouestware.com/en/">OuestWare</a>, using{" "}
-        <a href="https://reactjs.org/">react</a> and <a href="https://sigmajs.org">sigma.js</a>. You can read the source
-        code <a href="https://github.com/jacomyal/sigma.js/tree/main/demo">on GitHub</a>.
+        This web application has been developed by{" "}
+        <a target="_blank" rel="noreferrer" href="https://www.ouestware.com/en/">
+          OuestWare
+        </a>
+        , using{" "}
+        <a target="_blank" rel="noreferrer" href="https://reactjs.org/">
+          react
+        </a>{" "}
+        and{" "}
+        <a target="_blank" rel="noreferrer" href="https://sigmajs.org">
+          sigma.js
+        </a>
+        . You can read the source code{" "}
+        <a target="_blank" rel="noreferrer" href="https://github.com/jacomyal/sigma.js/tree/main/demo">
+          on GitHub
+        </a>
+        .
       </p>
-      <hr />
       <p>
-        You can click a node to open the related Wikipedia article. Also, a node size is related to its degree in the
-        network (bigger nodes are more connected).
+        Nodes sizes are related to their{" "}
+        <a target="_blank" rel="noreferrer" href="https://en.wikipedia.org/wiki/Betweenness_centrality">
+          betweenness centrality
+        </a>
+        . More central nodes (ie. bigger nodes) are important crossing points in the network. Finally, You can click a
+        node to open the related Wikipedia article
       </p>
     </Panel>
   );
--- demo/src/views/GraphDataController.tsx
+++ demo/src/views/GraphDataController.tsx
@@ -27,16 +27,18 @@ const GraphDataController: FC<{ dataset: Dataset; filters: FiltersState }> = ({ 
     dataset.edges.forEach(([source, target]) => graph.addEdge(source, target, { size: 1 }));
 
     // Use degrees as node sizes:
-    const degrees = graph.nodes().map((node) => graph.degree(node));
-    const minDegree = Math.min(...degrees);
-    const maxDegree = Math.max(...degrees);
+    const scores = graph.nodes().map((node) => graph.getNodeAttribute(node, "score"));
+    const minDegree = Math.min(...scores);
+    const maxDegree = Math.max(...scores);
     const MIN_NODE_SIZE = 3;
     const MAX_NODE_SIZE = 30;
     graph.forEachNode((node) =>
       graph.setNodeAttribute(
         node,
         "size",
-        ((graph.degree(node) - minDegree) / (maxDegree - minDegree)) * (MAX_NODE_SIZE - MIN_NODE_SIZE) + MIN_NODE_SIZE,
+        ((graph.getNodeAttribute(node, "score") - minDegree) / (maxDegree - minDegree)) *
+          (MAX_NODE_SIZE - MIN_NODE_SIZE) +
+          MIN_NODE_SIZE,
       ),
     );
 
