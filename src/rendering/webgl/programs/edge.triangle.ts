a3b5de1e6cb2531562825fb136e9ebb8b9206d60
Alexis Jacomy <alexis.jacomy@gmail.com>
2021-10-22 15:24:08 +0200

    Fixes edge.triangle program
    Same as previous commit: this fixes the edge.triangle program to get
    proper thickness (only touching the source node here, since the edge is
    a triangle).

--- src/rendering/webgl/programs/edge.triangle.ts
+++ src/rendering/webgl/programs/edge.triangle.ts
@@ -2,29 +2,27 @@
  * Sigma.js WebGL Renderer Triangle Edge Program
  * ==============================================
  *
- * Program rendering directed edges as a single anti-aliased triangle.
+ * Program rendering directed edges as a single triangle.
  * @module
  */
-import { EdgeDisplayData, NodeDisplayData } from "../../../types";
 import { floatColor } from "../../../utils";
+import { EdgeDisplayData, NodeDisplayData } from "../../../types";
 import vertexShaderSource from "../shaders/edge.triangle.vert.glsl";
 import fragmentShaderSource from "../shaders/edge.triangle.frag.glsl";
 import { AbstractEdgeProgram } from "./common/edge";
 import { RenderParams } from "./common/program";
 
 const POINTS = 3,
-  ATTRIBUTES = 9;
+  ATTRIBUTES = 5,
+  STRIDE = POINTS * ATTRIBUTES;
 
 export default class EdgeTriangleProgram extends AbstractEdgeProgram {
   positionLocation: GLint;
   colorLocation: GLint;
   normalLocation: GLint;
-  thicknessLocation: GLint;
-  barycentricLocation: GLint;
   matrixLocation: WebGLUniformLocation;
-  resolutionLocation: WebGLUniformLocation;
-  ratioLocation: WebGLUniformLocation;
-  scaleLocation: WebGLUniformLocation;
+  sqrtZoomRatioLocation: WebGLUniformLocation;
+  correctionRatioLocation: WebGLUniformLocation;
 
   constructor(gl: WebGLRenderingContext) {
     super(gl, vertexShaderSource, fragmentShaderSource, POINTS, ATTRIBUTES);
@@ -33,25 +31,20 @@ export default class EdgeTriangleProgram extends AbstractEdgeProgram {
     this.positionLocation = gl.getAttribLocation(this.program, "a_position");
     this.colorLocation = gl.getAttribLocation(this.program, "a_color");
     this.normalLocation = gl.getAttribLocation(this.program, "a_normal");
-    this.thicknessLocation = gl.getAttribLocation(this.program, "a_thickness");
-    this.barycentricLocation = gl.getAttribLocation(this.program, "a_barycentric");
 
-    // Uniform locations
     const matrixLocation = gl.getUniformLocation(this.program, "u_matrix");
     if (matrixLocation === null) throw new Error("EdgeTriangleProgram: error while getting matrixLocation");
     this.matrixLocation = matrixLocation;
 
-    const resolutionLocation = gl.getUniformLocation(this.program, "u_resolution");
-    if (resolutionLocation === null) throw new Error("EdgeTriangleProgram: error while getting resolutionLocation");
-    this.resolutionLocation = resolutionLocation;
+    const correctionRatioLocation = gl.getUniformLocation(this.program, "u_correctionRatio");
+    if (correctionRatioLocation === null)
+      throw new Error("EdgeTriangleProgram: error while getting correctionRatioLocation");
+    this.correctionRatioLocation = correctionRatioLocation;
 
-    const ratioLocation = gl.getUniformLocation(this.program, "u_ratio");
-    if (ratioLocation === null) throw new Error("EdgeTriangleProgram: error while getting ratioLocation");
-    this.ratioLocation = ratioLocation;
-
-    const scaleLocation = gl.getUniformLocation(this.program, "u_scale");
-    if (scaleLocation === null) throw new Error("EdgeTriangleProgram: error while getting scaleLocation");
-    this.scaleLocation = scaleLocation;
+    const sqrtZoomRatioLocation = gl.getUniformLocation(this.program, "u_sqrtZoomRatio");
+    if (sqrtZoomRatioLocation === null)
+      throw new Error("EdgeTriangleProgram: error while getting sqrtZoomRatioLocation");
+    this.sqrtZoomRatioLocation = sqrtZoomRatioLocation;
 
     this.bind();
   }
@@ -62,21 +55,17 @@ export default class EdgeTriangleProgram extends AbstractEdgeProgram {
     // Bindings
     gl.enableVertexAttribArray(this.positionLocation);
     gl.enableVertexAttribArray(this.normalLocation);
-    gl.enableVertexAttribArray(this.thicknessLocation);
     gl.enableVertexAttribArray(this.colorLocation);
-    gl.enableVertexAttribArray(this.barycentricLocation);
 
     gl.vertexAttribPointer(this.positionLocation, 2, gl.FLOAT, false, ATTRIBUTES * Float32Array.BYTES_PER_ELEMENT, 0);
     gl.vertexAttribPointer(this.normalLocation, 2, gl.FLOAT, false, ATTRIBUTES * Float32Array.BYTES_PER_ELEMENT, 8);
-    gl.vertexAttribPointer(this.thicknessLocation, 1, gl.FLOAT, false, ATTRIBUTES * Float32Array.BYTES_PER_ELEMENT, 16);
-    gl.vertexAttribPointer(this.colorLocation, 1, gl.FLOAT, false, ATTRIBUTES * Float32Array.BYTES_PER_ELEMENT, 20);
     gl.vertexAttribPointer(
-      this.barycentricLocation,
-      3,
-      gl.FLOAT,
-      false,
+      this.colorLocation,
+      4,
+      gl.UNSIGNED_BYTE,
+      true,
       ATTRIBUTES * Float32Array.BYTES_PER_ELEMENT,
-      24,
+      16,
     );
   }
 
@@ -87,9 +76,9 @@ export default class EdgeTriangleProgram extends AbstractEdgeProgram {
     hidden: boolean,
     offset: number,
   ): void {
-    let i = 0;
     if (hidden) {
-      for (let l = i + POINTS * ATTRIBUTES; i < l; i++) this.array[i] = 0;
+      for (let i = offset * STRIDE, l = i + STRIDE; i < l; i++) this.array[i] = 0;
+      return;
     }
 
     const thickness = data.size || 1,
@@ -110,48 +99,32 @@ export default class EdgeTriangleProgram extends AbstractEdgeProgram {
     if (len) {
       len = 1 / Math.sqrt(len);
 
-      n1 = -dy * len;
-      n2 = dx * len;
+      n1 = -dy * len * thickness;
+      n2 = dx * len * thickness;
     }
 
-    i = POINTS * ATTRIBUTES * offset;
+    let i = POINTS * ATTRIBUTES * offset;
 
     const array = this.array;
-
-    // TODO: I guess it's not necessary to pass normals to the shader here
 
     // First point
     array[i++] = x1;
     array[i++] = y1;
     array[i++] = n1;
     array[i++] = n2;
-    array[i++] = thickness;
     array[i++] = color;
-    array[i++] = 1;
-    array[i++] = 0;
-    array[i++] = 0;
 
-    // Second point
     array[i++] = x1;
     array[i++] = y1;
     array[i++] = -n1;
     array[i++] = -n2;
-    array[i++] = thickness;
     array[i++] = color;
-    array[i++] = 0;
-    array[i++] = 1;
-    array[i++] = 0;
 
-    // Third point
     array[i++] = x2;
     array[i++] = y2;
     array[i++] = 0;
     array[i++] = 0;
-    array[i++] = 0;
-    array[i++] = color;
-    array[i++] = 0;
-    array[i++] = 0;
-    array[i] = 20;
+    array[i] = color;
   }
 
   computeIndices(): void {
@@ -160,17 +133,13 @@ export default class EdgeTriangleProgram extends AbstractEdgeProgram {
 
   render(params: RenderParams): void {
     const gl = this.gl;
-
     const program = this.program;
+
     gl.useProgram(program);
 
-    // Binding uniforms
-    gl.uniform2f(this.resolutionLocation, params.width, params.height);
-    gl.uniform1f(this.ratioLocation, params.ratio / Math.sqrt(params.ratio));
-
     gl.uniformMatrix3fv(this.matrixLocation, false, params.matrix);
-
-    gl.uniform1f(this.scaleLocation, params.ratio);
+    gl.uniform1f(this.sqrtZoomRatioLocation, Math.sqrt(params.ratio));
+    gl.uniform1f(this.correctionRatioLocation, params.correctionRatio);
 
     // Drawing:
     gl.drawArrays(gl.TRIANGLES, 0, this.array.length / ATTRIBUTES);
--- src/rendering/webgl/shaders/edge.triangle.vert.glsl
+++ src/rendering/webgl/shaders/edge.triangle.vert.glsl
@@ -1,36 +1,29 @@
-attribute vec2 a_position;
+attribute vec4 a_color;
 attribute vec2 a_normal;
-attribute float a_thickness;
-attribute float a_color;
-attribute vec3 a_barycentric;
+attribute vec2 a_position;
 
-uniform vec2 u_resolution;
-uniform float u_ratio;
 uniform mat3 u_matrix;
-uniform float u_scale;
+uniform float u_sqrtZoomRatio;
+uniform float u_correctionRatio;
 
 varying vec4 v_color;
-varying vec3 v_barycentric;
 
-const float feather = 0.0;
+const float minThickness = 1.7;
+const float bias = 255.0 / 254.0;
 
 void main() {
+  // The only different here with edge.vert.glsl is that we need to handle null
+  // input normal vector. Apart from that, you can read edge.vert.glsl more info
+  // on how it works:
+  float normalLength = length(a_normal);
+  vec2 unitNormal = a_normal / normalLength;
+  if (normalLength <= 0.0) unitNormal = a_normal;
+  float pixelsThickness = max(normalLength, minThickness * u_sqrtZoomRatio);
+  float webGLThickness = pixelsThickness * u_correctionRatio;
+  float adaptedWebGLThickness = webGLThickness * u_sqrtZoomRatio;
 
-  // Scale from [[-1 1] [-1 1]] to the container:
-  float size = a_thickness * u_ratio + feather;
-  vec2 delta = vec2(a_normal * size);
-  vec2 position = (u_matrix * vec3(a_position + delta, 1)).xy;
-  position = (position / u_resolution * 2.0 - 1.0) * vec2(1, -1);
+  gl_Position = vec4((u_matrix * vec3(a_position + unitNormal * adaptedWebGLThickness, 1)).xy, 0, 1);
 
-  // Applying
-  gl_Position = vec4(position, 0, 1);
-
-  v_barycentric = a_barycentric;
-
-  // Extract the color:
-  float c = a_color;
-  v_color.b = mod(c, 256.0); c = floor(c / 256.0);
-  v_color.g = mod(c, 256.0); c = floor(c / 256.0);
-  v_color.r = mod(c, 256.0); c = floor(c / 256.0); v_color /= 255.0;
-  v_color.a = 1.0;
+  v_color = a_color;
+  v_color.a *= bias;
 }
