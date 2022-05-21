b615957ea210f6f3709e93a83aa461410d43751d
Seph Reed <ShadyWillowCreek@gmail.com>
2022-05-21 01:38:02 -0600

    Direction for missing "nodeProgramClasses" entry
    I copied an example project, and decided I didn't feel like copying over all the different `nodeProgramClasses`.  When I ran my project, I got a strange error with an untraceable call stack.  By adding this small check, no such confusion will ever have to happen again.
    
    > main.ts:8 Uncaught TypeError: Cannot read properties of undefined (reading 'process')
    >    at Sigma.process (main.ts:8:40)
    >    at new Sigma (main.ts:8:40)
    >    at SigmaSentimentGraph.initRenderer (SigmaGraph.view.ts:80:10)
    >    at SentimentMatrix.view.ts:12:26

--- src/sigma.ts
+++ src/sigma.ts
@@ -784,7 +784,12 @@ export default class Sigma<GraphType extends Graph = Graph> extends TypedEventEm
       if (typeof data.label === "string" && !data.hidden)
         this.labelGrid.add(node, data.size, this.framedGraphToViewport(data, { matrix: nullCameraMatrix }));
 
-      this.nodePrograms[data.type].process(data, data.hidden, nodesPerPrograms[data.type]++);
+      const nodeProgram = this.nodePrograms[data.type];
+      if (nodeProgram) {
+        nodeProgram.process(data, data.hidden, nodesPerPrograms[data.type]++);
+      } else {
+        console.error(`Node Program of type "${data.type}" specified by node, but not included in "nodeProgramClasses" field of Sigma configuration`);
+      }
 
       // Save the node in the highlighted set if needed
       if (data.highlighted && !data.hidden) this.highlightedNodes.add(node);
