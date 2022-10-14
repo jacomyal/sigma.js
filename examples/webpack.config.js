8704936931c2eba4ee11c1389db98a9258b50f9c
Yomguithereal <guillaumeplique@gmail.com>
2022-10-14 13:54:36 +0200

    Moving examples tsconfig Moving the config into the examples folder fixes the VSCode TS inference

--- examples/webpack.config.js
+++ examples/webpack.config.js
@@ -34,9 +34,6 @@ module.exports = {
         test: /\.ts$/,
         exclude: /node_modules/,
         loader: "ts-loader",
-        options: {
-          configFile: path.resolve(__dirname, "../tsconfig.examples.json"),
-        },
       },
     ],
   },
