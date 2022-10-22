f6473294dae7ffa550c5685f5a7eff8d57cc840e
Yomguithereal <guillaumeplique@gmail.com>
2022-10-22 13:56:03 +0200

    Refinining proposition

--- src/rendering/webgl/programs/common/proposition.ts
+++ src/rendering/webgl/programs/common/proposition.ts
@@ -1,18 +1,40 @@
+import type Sigma from "../../../../sigma";
+import type { NodeDisplayData } from "../../../../types";
+import type { RenderParams } from "./program";
+import { floatColor } from "../../../../utils";
 import { loadVertexShader, loadFragmentShader, loadProgram } from "../../shaders/utils";
+
+// ---------------------------------------------------------------------
+// Minimum viable class to implement for your program to work with Sigma
+// ---------------------------------------------------------------------
+export abstract class AbstractNodeProgram {
+  constructor(_gl: WebGLRenderingContext, _renderer: Sigma) {}
+  abstract reallocate(capacity: number): void;
+  abstract process(offset: number, data: NodeDisplayData, hidden?: boolean): void;
+  abstract render(params: RenderParams): void;
+}
+
+// --------------------------
+// Typical Sigma node program
+// --------------------------
+
+// TODO: edge vs. node
+// TODO: indices
 
 export type AttributeSpecification = {
   name: string;
   size: number;
-  type: "float" | "unsigned_byte";
+  type: number;
 };
 
-export abstract class NodeProgram {
+export abstract class NodeProgram implements AbstractNodeProgram {
   abstract readonly VERTICES: number;
   abstract readonly ARRAY_ITEMS_PER_VERTEX: number;
   abstract readonly VERTEX_SHADER_SOURCE: string;
   abstract readonly FRAGMENT_SHADER_SOURCE: string;
   abstract readonly UNIFORMS: Array<string>;
   abstract readonly ATTRIBUTES: Array<AttributeSpecification>;
+  STRIDE: number = 0;
 
   gl: WebGLRenderingContext;
   buffer: WebGLBuffer;
@@ -23,7 +45,7 @@ export abstract class NodeProgram {
   uniformLocations: Record<string, WebGLUniformLocation> = {};
   attributeLocations: Record<string, GLint> = {};
   capacity = 0;
-  vertices = 0;
+  verticesCount = 0;
 
   constructor(gl: WebGLRenderingContext) {
     this.gl = gl;
@@ -39,6 +61,7 @@ export abstract class NodeProgram {
     this.fragmentShader = fragmentShader;
     this.program = program;
 
+    this.initMembers();
     this.initLocations();
     this.bind();
   }
@@ -49,6 +72,10 @@ export abstract class NodeProgram {
     const program = loadProgram(this.gl, [this.vertexShader, this.fragmentShader]);
 
     return [vertexShader, fragmentShader, program];
+  }
+
+  private initMembers() {
+    this.STRIDE = this.VERTICES * this.ARRAY_ITEMS_PER_VERTEX;
   }
 
   private initLocations() {
@@ -65,32 +92,67 @@ export abstract class NodeProgram {
     }
   }
 
-  bind(): void {
+  private bind(): void {
     const gl = this.gl;
 
     for (const attributeName in this.ATTRIBUTES) {
       gl.enableVertexAttribArray(this.attributeLocations[attributeName]);
     }
 
-    // TODO: strides
+    // TODO: vertexAttribPointer etc. that strides automagically (I don't want
+    // to manage this by hand anymore).
   }
 
-  bufferData(): void {
+  private bufferData(): void {
     const gl = this.gl;
     gl.bufferData(gl.ARRAY_BUFFER, this.array, gl.DYNAMIC_DRAW);
   }
 
-  allocate(capacity: number): void {
+  reallocate(capacity: number): void {
+    // NOTE: we should test here capacity changes and assess whether arrays
+    // must be dynamically reallocated or not. This makes the keepArrays
+    // argument of renderer.process useless and we can drop the
+    // soft process vs. hard process difference which should simplify the
+    // renderer's lifecycle.
+
+    if (capacity === this.capacity) return;
+
     this.capacity = capacity;
-    this.vertices = this.VERTICES * capacity;
-    this.array = new Float32Array(this.vertices * this.ARRAY_ITEMS_PER_VERTEX);
+    this.verticesCount = this.VERTICES * capacity;
+    this.array = new Float32Array(this.verticesCount * this.ARRAY_ITEMS_PER_VERTEX);
   }
 
   hasNothingToRender(): boolean {
     return this.array.length === 0;
   }
 
-  // TODO: process, processAll, render, draw
+  process(offset: number, data: NodeDisplayData, hidden?: boolean): void {
+    let i = offset * this.STRIDE;
+
+    // NOTE: dealing with the hidden issues automatically
+    if (!hidden) return this.processItem(this.array, i, data);
+
+    for (let l = i + this.STRIDE; i < l; i++) {
+      this.array[i] = 0;
+    }
+  }
+
+  // NOTE: I chose to explicitly pass members of the class to the methods
+  // such as this.gl etc. but this could be dropped, I just feel it makes
+  // the class easier to implement, regarding our usual practice
+  abstract processItem(array: Float32Array, i: number, data: NodeDisplayData): void;
+  abstract setUniforms(gl: WebGLRenderingContext, params: RenderParams): void;
+  abstract draw(gl: WebGLRenderingContext, params: RenderParams): void;
+
+  render(params: RenderParams): void {
+    if (this.hasNothingToRender()) return;
+
+    this.bind();
+    this.bufferData();
+    this.gl.useProgram(this.program);
+    this.setUniforms(this.gl, params);
+    this.draw(this.gl, params);
+  }
 }
 
 // -------
@@ -99,6 +161,8 @@ export abstract class NodeProgram {
 import vertexShaderSource from "../shaders/node.vert.glsl";
 import fragmentShaderSource from "../shaders/node.frag.glsl";
 
+const { FLOAT, UNSIGNED_BYTE } = WebGLRenderingContext;
+
 export class NodeFastProgram extends NodeProgram {
   readonly VERTICES = 1;
   readonly ARRAY_ITEMS_PER_VERTEX = 5;
@@ -106,9 +170,28 @@ export class NodeFastProgram extends NodeProgram {
   readonly FRAGMENT_SHADER_SOURCE = fragmentShaderSource;
   readonly UNIFORMS = ["u_matrix", "u_sqrtZoomRatio", "u_correctionRatio"];
   readonly ATTRIBUTES = [
-    { name: "a_position", size: 2, type: "float" as const },
-    { name: "a_size", size: 1, type: "float" as const },
-    { name: "a_color", size: 4, type: "unsigned_byte" as const },
-    { name: "a_angle", size: 1, type: "float" as const },
+    { name: "a_position", size: 2, type: FLOAT },
+    { name: "a_size", size: 1, type: FLOAT },
+    { name: "a_color", size: 4, type: UNSIGNED_BYTE },
+    { name: "a_angle", size: 1, type: FLOAT },
   ];
+
+  processItem(array: Float32Array, i: number, data: NodeDisplayData): void {
+    const color = floatColor(data.color);
+
+    array[i++] = data.x;
+    array[i++] = data.y;
+    array[i++] = data.size;
+    array[i] = color;
+  }
+
+  setUniforms(gl: WebGLRenderingContext, params: RenderParams): void {
+    gl.uniformMatrix3fv(this.uniformLocations.u_matrix, false, params.matrix);
+    gl.uniform1f(this.uniformLocations.u_sqrtZoomRatioLocation, Math.sqrt(params.ratio));
+    gl.uniform1f(this.uniformLocations.u_correctionRatioLocation, params.correctionRatio);
+  }
+
+  draw(gl: WebGLRenderingContext, _params: RenderParams): void {
+    gl.drawArrays(gl.TRIANGLES, 0, this.verticesCount);
+  }
 }
