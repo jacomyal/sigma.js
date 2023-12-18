506d5034eaf30b2c7dff21c4a4bf2d6b99ca7166
Alexis Jacomy <alexis.jacomy@gmail.com>
2023-12-18 07:36:57 +0100

    [sigma] Fixes shaders transpilation

--- packages/sigma/webpack.programs.config.js
+++ packages/sigma/webpack.programs.config.js
@@ -1,28 +1,28 @@
-let path = require('path'),
-  glob = require('glob');
+let path = require("path"),
+  glob = require("glob");
 
-const shaders = glob.sync(path.join(__dirname, 'src', 'rendering', 'webgl', 'shaders', '*.glsl'));
+const shaders = glob.sync(path.join(__dirname, "src/rendering/programs/**/*.glsl"));
 
 const entry = {};
 
 shaders.forEach(function (p) {
-  entry[path.basename(p, '.glsl')] = p;
+  entry[path.join(path.basename(path.dirname(p)), path.basename(p, ".glsl"))] = p;
 });
 
 module.exports = {
-  mode: 'production',
+  mode: "production",
   entry,
   output: {
-    path: path.join(__dirname, 'rendering', 'webgl', 'shaders'),
-    filename: '[name].glsl.js',
-    libraryTarget: 'commonjs2',
+    path: path.join(__dirname, "rendering/programs"),
+    filename: "[name].glsl.js",
+    libraryTarget: "commonjs2",
   },
   module: {
     rules: [
       {
         test: /\.glsl$/,
         exclude: /node_modules/,
-        loader: 'raw-loader',
+        loader: "raw-loader",
       },
     ],
   },
