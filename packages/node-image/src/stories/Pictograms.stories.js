d4a46ebcc322d9923500a28ba64871205bf408bb
Alexis Jacomy <alexis.jacomy@gmail.com>
2024-01-22 11:25:18 +0100

    [node-image] Migrates to Storybook
    WARNING: At this point, the Storybook does not work properly, because it
    does not use the proper files from sigma (it uses the sources instead of
    the built version).
    
    The new build is based on the following blog post:
    https://betterprogramming.pub/getting-started-with-storybook-without-a-javascript-framework-c2968d3f3d9f

---
+++ packages/node-image/src/stories/Pictograms.stories.js
@@ -0,0 +1,12 @@
+import { createPictogramsStage } from "./Pictograms.js";
+
+export default {
+  title: "node-image/Pictograms",
+  render: () => createPictogramsStage(),
+  parameters: {
+    // More on how to position stories at: https://storybook.js.org/docs/configure/story-layout
+    layout: "fullscreen",
+  },
+};
+
+export const Default = {};
