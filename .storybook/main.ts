11709e1eec5d13b344f3d3420e96145e899a1cdc
Alexis Jacomy <alexis.jacomy@gmail.com>
2024-01-31 17:55:48 +0100

    [node-image] Fixes StoryBook
    Fixes Vite configuration for StoryBook so that the example runs properly
    in both Chrome and Firefox.

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,10 +1,16 @@
+import { dirname, join } from "path";
 import type { StorybookConfig } from "@storybook/html-vite";
+import { mergeConfig } from "vite";
 
 /** @type { import('@storybook/html-vite').StorybookConfig } */
 const config: StorybookConfig = {
   stories: ["../packages/**/src/**/*.mdx", "../packages/**/src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
-  addons: ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions"],
-  framework: "@storybook/html-vite",
+  addons: [
+    getAbsolutePath("@storybook/addon-links"),
+    getAbsolutePath("@storybook/addon-essentials"),
+    getAbsolutePath("@storybook/addon-interactions"),
+  ],
+  framework: getAbsolutePath("@storybook/html-vite"),
   typescript: {
     check: true,
     skipBabel: false,
@@ -13,5 +19,16 @@ const config: StorybookConfig = {
     autodocs: "tag",
   },
   logLevel: "debug",
+  async viteFinal(config) {
+    return mergeConfig(config, {
+      resolve: {
+        preserveSymlinks: true,
+      },
+    });
+  },
 };
 export default config;
+
+function getAbsolutePath(value: string): any {
+  return dirname(require.resolve(join(value, "package.json")));
+}
--- packages/node-image/vite.config.ts
+++ packages/node-image/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import dts from "vite-plugin-dts";
+
 import { name, peerDependencies } from "./package.json";
 
 export default defineConfig({
@@ -8,7 +9,7 @@ export default defineConfig({
       name, // Sets the name of the generated library.
       entry: "./src/index.ts", // Specifies the entry point for building the library.
       fileName: (format) => `index.${format}.js`, // Generates the output file name based on the format.
-      formats: ["cjs", "es"], // Specifies the output formats (CommonJS and ES modules).
+      formats: ["es"], // Specifies the output formats (CommonJS and ES modules).
     },
     rollupOptions: {
       external: [...Object.keys(peerDependencies)], // Defines external dependencies for Rollup bundling.
@@ -22,5 +23,8 @@ export default defineConfig({
   plugins: [dts()], // Uses the 'vite-plugin-dts' plugin for generating TypeScript declaration files (d.ts).
   optimizeDeps: {
     include: ["sigma"],
+  },
+  resolve: {
+    preserveSymlinks: true,
   },
 });
