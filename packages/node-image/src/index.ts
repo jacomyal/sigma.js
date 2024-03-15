806a29a23c977617bd1df8c5bfccf769b984455a
Alexis Jacomy <alexis.jacomy@gmail.com>
2024-03-15 16:31:39 +0100

    [node-image] Fixes NodePictogramProgram options

--- packages/node-image/src/index.ts
+++ packages/node-image/src/index.ts
@@ -3,6 +3,7 @@ import createNodeImageProgram from "./factory";
 export { default as createNodeImageProgram } from "./factory";
 export const NodeImageProgram = createNodeImageProgram();
 export const NodePictogramProgram = createNodeImageProgram({
+  keepWithinCircle: false,
   size: { mode: "force", value: 256 },
   drawingMode: "color",
   correctCentering: true,
