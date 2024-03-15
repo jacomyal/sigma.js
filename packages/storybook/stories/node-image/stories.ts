e855203f988da37a76cbbe09b0e762696bcfeb25
Alexis Jacomy <alexis.jacomy@gmail.com>
2024-03-15 16:32:38 +0100

    [storybook] Improves @sigma/node-image stories

--- packages/storybook/stories/node-image/stories.ts
+++ packages/storybook/stories/node-image/stories.ts
@@ -1,8 +1,14 @@
 import type { Meta, StoryObj } from "@storybook/web-components";
 
-import play from "./index";
 import template from "./index.html?raw";
-import source from "./index?raw";
+import nodeImagesPlay from "./node-images";
+import nodeImagesSource from "./node-images?raw";
+import nodePictogramsPlay from "./node-pictograms";
+import nodePictogramsBackgroundPlay from "./node-pictograms-background";
+import nodePictogramsBackgroundSource from "./node-pictograms-background?raw";
+import nodePictogramsSource from "./node-pictograms?raw";
+import optionsShowcasePlay from "./options-showcase";
+import optionsShowcaseSource from "./options-showcase?raw";
 
 const meta: Meta = {
   id: "node-image",
@@ -12,14 +18,50 @@ export default meta;
 
 type Story = StoryObj;
 
-export const story: Story = {
-  name: "All features",
+export const nodeImages: Story = {
+  name: "NodeImageRenderer",
   render: () => template,
-  play,
+  play: nodeImagesPlay,
   args: {},
   parameters: {
     storySource: {
-      source,
+      source: nodeImagesSource,
+    },
+  },
+};
+
+export const nodePictograms: Story = {
+  name: "NodePictogramRenderer",
+  render: () => template,
+  play: nodePictogramsPlay,
+  args: {},
+  parameters: {
+    storySource: {
+      source: nodePictogramsSource,
+    },
+  },
+};
+
+export const nodePictogramsWithBackground: Story = {
+  name: "NodePictogramRenderer with background colors",
+  render: () => template,
+  play: nodePictogramsBackgroundPlay,
+  args: {},
+  parameters: {
+    storySource: {
+      source: nodePictogramsBackgroundSource,
+    },
+  },
+};
+
+export const optionsShowcase: Story = {
+  name: "Options showcase",
+  render: () => template,
+  play: optionsShowcasePlay,
+  args: {},
+  parameters: {
+    storySource: {
+      source: optionsShowcaseSource,
     },
   },
 };
