bc713469a2000288fda986c36b1c71c49322dbd4
Alexis Jacomy <alexis.jacomy@gmail.com>
2024-03-22 11:23:11 +0100

    [storybook] Adds Matomo tracking

--- packages/storybook/.storybook/manager.tsx
+++ packages/storybook/.storybook/manager.tsx
@@ -2,6 +2,10 @@ import { addons } from "@storybook/manager-api";
 
 import theme from "./theme";
 
+const image = document.createElement("img") as HTMLImageElement;
+image.src = "https://matomo.ouestware.com/matomo.php?idsite=26&rec=1&action_name=Storybook&send_image=0";
+document.body.append(image);
+
 addons.setConfig({
   theme,
 });
