b3783ab41ee13a4039e84de10f41abb924d9453c
Alexis Jacomy <alexis.jacomy@gmail.com>
2024-03-28 11:48:08 +0100

    [node-border] Optimizes shaders
    Details:
    - Moves the borders color and size selection from the vertex shader to
      the fragment shader, to minimize the number of varyings used
    - Refactors a bit the GLSL code so that it is a bit easier to maintain

--- packages/node-border/src/shader-frag.ts
+++ packages/node-border/src/shader-frag.ts
@@ -1,19 +1,29 @@
-import { CreateNodeBorderProgramOptions } from "./utils";
+import { CreateNodeBorderProgramOptions, DEFAULT_BORDER_SIZE_MODE, NodeBorderSize, numberToGLSLFloat } from "./utils";
 
 export default function getFragmentShader({ borders }: CreateNodeBorderProgramOptions) {
+  const fillCounts = numberToGLSLFloat(borders.filter(({ size }) => "fill" in size).length);
+
   // language=GLSL
   const SHADER = /*glsl*/ `
 precision highp float;
 
 varying vec2 v_diffVector;
 varying float v_radius;
-${borders.map((_, i) => `varying float v_borderSize_${i + 1};`).join("\n")}
 
 #ifdef PICKING_MODE
 varying vec4 v_color;
 #else
 // For normal mode, we use the border colors defined in the program:
-${borders.map((_, i) => `varying vec4 v_borderColor_${i + 1};`).join("\n")}
+${borders.flatMap(({ size }, i) => ("attribute" in size ? [`varying float v_borderSize_${i + 1};`] : [])).join("\n")}
+${borders
+  .flatMap(({ color }, i) =>
+    "attribute" in color
+      ? [`varying vec4 v_borderColor_${i + 1};`]
+      : "value" in color
+        ? [`uniform vec4 u_borderColor_${i + 1};`]
+        : [],
+  )
+  .join("\n")}
 #endif
 
 uniform float u_correctionRatio;
@@ -31,18 +41,59 @@ void main(void) {
   #ifdef PICKING_MODE
   if (dist > v_radius)
     gl_FragColor = transparent;
-  else
+  else {
     gl_FragColor = v_color;
-
+    gl_FragColor.a *= bias;
+  }
   #else
-  if (dist > v_borderSize_0) {
-    gl_FragColor = v_borderColor_0;
+  // Sizes:
+${borders
+  .flatMap(({ size }, i) => {
+    if ("fill" in size) return [];
+
+    size = size as Exclude<NodeBorderSize, { fill: true }>;
+    const value = "attribute" in size ? `v_borderSize_${i + 1}` : numberToGLSLFloat(size.value);
+    const factor = (size.mode || DEFAULT_BORDER_SIZE_MODE) === "pixels" ? "u_correctionRatio" : "v_radius";
+    return [`  float borderSize_${i + 1} = ${factor} * ${value};`];
+  })
+  .join("\n")}
+  // Now, let's split the remaining space between "fill" borders:
+  float fillBorderSize = (v_radius - (${borders
+    .flatMap(({ size }, i) => (!("fill" in size) ? [`borderSize_${i + 1}`] : []))
+    .join(" + ")}) ) / ${fillCounts};
+${borders.flatMap(({ size }, i) => ("fill" in size ? [`  float borderSize_${i + 1} = fillBorderSize;`] : [])).join("\n")}
+
+  // Finally, normalize all border sizes, to start from the full size and to end with the smallest:
+  float adjustedBorderSize_0 = v_radius;
+${borders.map((_, i) => `  float adjustedBorderSize_${i + 1} = adjustedBorderSize_${i} - borderSize_${i + 1};`).join("\n")}
+
+  // Colors:
+  vec4 borderColor_0 = transparent;
+${borders
+  .map(({ color }, i) => {
+    const res: string[] = [];
+    if ("attribute" in color) {
+      res.push(`  vec4 borderColor_${i + 1} = v_borderColor_${i + 1};`);
+    } else if ("transparent" in color) {
+      res.push(`  vec4 borderColor_${i + 1} = vec4(0.0, 0.0, 0.0, 0.0);`);
+    } else {
+      res.push(`  vec4 borderColor_${i + 1} = u_borderColor_${i + 1};`);
+    }
+
+    res.push(`  borderColor_${i + 1}.a *= bias;`);
+    res.push(`  if (borderSize_${i + 1} <= 1.0 * u_correctionRatio) { borderColor_${i + 1} = borderColor_${i}; }`);
+
+    return res.join("\n");
+  })
+  .join("\n")}
+  if (dist > adjustedBorderSize_0) {
+    gl_FragColor = borderColor_0;
   } else ${borders
     .map(
-      (_, i) => `if (dist > v_borderSize_${i} - aaBorder) {
-    gl_FragColor = mix(v_borderColor_${i + 1}, v_borderColor_${i}, (dist - v_borderSize_${i} + aaBorder) / aaBorder);
-  } else if (dist > v_borderSize_${i + 1}) {
-    gl_FragColor = v_borderColor_${i + 1};
+      (_, i) => `if (dist > adjustedBorderSize_${i} - aaBorder) {
+    gl_FragColor = mix(borderColor_${i + 1}, borderColor_${i}, (dist - adjustedBorderSize_${i} + aaBorder) / aaBorder);
+  } else if (dist > adjustedBorderSize_${i + 1}) {
+    gl_FragColor = borderColor_${i + 1};
   } else `,
     )
     .join("")} { /* Nothing to add here */ }
--- packages/node-border/src/shader-vert.ts
+++ packages/node-border/src/shader-vert.ts
@@ -1,30 +1,33 @@
-import { CreateNodeBorderProgramOptions, DEFAULT_BORDER_SIZE_MODE, NodeBorderSize, numberToGLSLFloat } from "./utils";
+import { CreateNodeBorderProgramOptions } from "./utils";
 
 export default function getVertexShader({ borders }: CreateNodeBorderProgramOptions) {
-  const fillCounts = numberToGLSLFloat(borders.filter(({ size }) => "fill" in size).length);
   // language=GLSL
   const SHADER = /*glsl*/ `
-attribute vec4 a_id;
 attribute vec2 a_position;
 attribute float a_size;
 attribute float a_angle;
-${borders.flatMap(({ size }, i) => ("attribute" in size ? [`attribute float a_borderSize_${i + 1};`] : [])).join("\n")}
-${borders.flatMap(({ color }, i) => ("attribute" in color ? [`attribute vec4 a_borderColor_${i + 1};`] : [])).join("\n")}
 
 uniform mat3 u_matrix;
 uniform float u_sizeRatio;
 uniform float u_correctionRatio;
-${borders.flatMap(({ color }, i) => ("value" in color ? [`uniform vec4 u_borderColor_${i + 1};`] : [])).join("\n")}
 
 varying vec2 v_diffVector;
 varying float v_radius;
-${borders.map((_, i) => `varying float v_borderSize_${i + 1};`).join("\n")}
 
 #ifdef PICKING_MODE
+attribute vec4 a_id;
 varying vec4 v_color;
 #else
-// For normal mode, we use the border colors defined in the program:
-${borders.map((_, i) => `varying vec4 v_borderColor_${i + 1};`).join("\n")}
+${borders
+  .flatMap(({ size }, i) =>
+    "attribute" in size ? [`attribute float a_borderSize_${i + 1};`, `varying float v_borderSize_${i + 1};`] : [],
+  )
+  .join("\n")}
+${borders
+  .flatMap(({ color }, i) =>
+    "attribute" in color ? [`attribute vec4 a_borderColor_${i + 1};`, `varying vec4 v_borderColor_${i + 1};`] : [],
+  )
+  .join("\n")}
 #endif
 
 const float bias = 255.0 / 254.0;
@@ -43,49 +46,14 @@ void main() {
   v_radius = size / 2.0;
   v_diffVector = diffVector;
 
-  // Let's first compute the size of "non-fill" borders:
-${borders
-  .flatMap(({ size }, i) => {
-    if ("fill" in size) return [];
-
-    size = size as Exclude<NodeBorderSize, { fill: true }>;
-    const value = "attribute" in size ? `a_borderSize_${i + 1}` : numberToGLSLFloat(size.value);
-    const factor = (size.mode || DEFAULT_BORDER_SIZE_MODE) === "pixels" ? "u_correctionRatio" : "v_radius";
-    return [`  float borderSize_${i + 1} = ${factor} * ${value};`];
-  })
-  .join("\n")}
-  // Now, let's split the remaining space between "fill" borders:
-  float fillBorderSize = (v_radius - (${borders
-    .flatMap(({ size }, i) => (!("fill" in size) ? [`borderSize_${i + 1}`] : []))
-    .join(" + ")}) ) / ${fillCounts};
-${borders.flatMap(({ size }, i) => ("fill" in size ? [`  float borderSize_${i + 1} = fillBorderSize;`] : [])).join("\n")}
-
-  // Finally, normalize all border sizes, to start from the full size and to end with the smallest:
-  float v_borderSize_0 = v_radius;
-${borders.map((_, i) => `  v_borderSize_${i + 1} = v_borderSize_${i} - borderSize_${i + 1};`).join("\n")}
-
   #ifdef PICKING_MODE
-  // For picking mode, we use the ID as the color:
   v_color = a_id;
-  v_color.a *= bias;
   #else
-  vec4 v_borderColor_0 = transparent;
 ${borders
-  .map(({ color }, i) => {
-    const res: string[] = [];
-    if ("attribute" in color) {
-      res.push(`  v_borderColor_${i + 1} = a_borderColor_${i + 1};`);
-    } else if ("transparent" in color) {
-      res.push(`  v_borderColor_${i + 1} = vec4(0.0, 0.0, 0.0, 0.0);`);
-    } else {
-      res.push(`  v_borderColor_${i + 1} = u_borderColor_${i + 1};`);
-    }
-
-    res.push(`  v_borderColor_${i + 1}.a *= bias;`);
-    res.push(`  if (borderSize_${i + 1} <= 1.0 * u_correctionRatio) { v_borderColor_${i + 1} = v_borderColor_${i}; }`);
-
-    return res.join("\n");
-  })
+  .flatMap(({ size }, i) => ("attribute" in size ? [`  v_borderSize_${i + 1} = a_borderSize_${i + 1};`] : []))
+  .join("\n")}
+${borders
+  .flatMap(({ color }, i) => ("attribute" in color ? [`  v_borderColor_${i + 1} = a_borderColor_${i + 1};`] : []))
   .join("\n")}
   #endif
 }
