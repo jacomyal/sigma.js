ccad61659d09c536c53ea695ce388d1127a2f5fe
Alexis Jacomy <alexis.jacomy@gmail.com>
2026-08-17 09:35:17 +0200

    [website] Documents satellite packages CDN usage
    This addresses the issue described in #1529.

--- packages/website/src/versions.ts
+++ packages/website/src/versions.ts
@@ -1,5 +1,7 @@
+import nodeImagePkg from "../../node-image/package.json";
 import sigmaPkg from "../../sigma/package.json";
 import websitePkg from "../package.json";
 
 export const SIGMA_VERSION = sigmaPkg.version;
+export const NODE_IMAGE_VERSION = nodeImagePkg.version;
 export const GRAPHOLOGY_VERSION = websitePkg.dependencies.graphology.replace(/^\D*/, "");
